fe2635b73163ef3eb3fe72fc08fc7ed675707d9d
Sowmya Parthiban <sparthi1@jh.edu>
2023-10-25 11:34:08 -0400

    try running IsoQuant on fastq

---
+++ code/02_IsoQuant/01_run_isoquant.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH -p shared
+#SBATCH --mem=150G
+#SBATCH --cpus-per-task=10
+#SBATCH --job-name=isoquant
+#SBATCH --mail-user=sparthi1@jhu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o logs/isoquant.%a.txt
+#SBATCH -e logs/isoquant.%a.txt
+#SBATCH --array=1-4
+
+#try running for all chromosomes
+
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+
+source activate isoquant 
+cd /users/sparthib/IsoQuant/
+
+CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
+sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
+INPUT_FASTQ=/dcs04/hicks/data/sparthib/casey/fastqs/${sample}.fastq.gz
+REFERENCE_GTF=/dcs04/hicks/data/sparthib/GENCODE_GTF.gtf.gz
+REFERENCE_FASTA=/dcs04/hicks/data/sparthib/GENCODE_FASTA.fa.gz 
+OUTPUT_FOLDER=/dcs04/hicks/data/sparthib/casey/IsoQuant_output/${sample}
+mkdir -p $OUTPUT_FOLDER
+
+isoquant.py --reference $REFERENCE_FASTA --data_type ont --genedb $REFERENCE_GTF --fastq $INPUT_FASTQ \
+  --output $OUTPUT_FOLDER --sqanti_output  --check_canonical --count_exons --clean_start \
+  --complete_genedb
+  
+conda deactivate
+
+echo "**** Job ends ****"
+jobstats
+date +"%Y-%m-%d %T"
