4b5fe8da88b7eaa4f0a0fae48d1df8778d23d7b5
Sowmya Parthiban <sparthi1@jh.edu>
2023-11-06 14:05:33 -0500

    add `\`

--- code/03_bam_QC/01_computeGCbias.sh
+++ code/03_bam_QC/01_computeGCbias.sh
@@ -26,8 +26,8 @@ source activate deeptools
 
 BAM_FOLDER=/dcs04/hicks/data/sparthib/casey/bams/hRGC_chr_21.bam
 
-computeGCBias -b $BAM_FOLDER --effectiveGenomeSize 2695000000
-   --genome genome.2bit -l 200 -freq freq_test.txt
+computeGCBias -b $BAM_FOLDER --effectiveGenomeSize 2695000000 \
+   --genome genome.2bit -l 200 -freq freq_test.txt \
    --region 21 --biasPlot test_gc.png
 
 
