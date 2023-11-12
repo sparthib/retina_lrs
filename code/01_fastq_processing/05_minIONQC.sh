32262f4171cd35ca43f6403c265b614658a55858
Sowmya Parthiban <sparthi1@jh.edu>
2023-11-12 16:03:36 -0500

    minIONQC shell script

---
+++ code/01_fastq_processing/05_minIONQC.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH --nodes=1
+#SBATCH --mem=250G
+#SBATCH -c 10
+#SBATCH --job-name=minIONQC
+#SBATCH --mail-user=sparthi1@jhu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o logs/minIONQC.%a.txt
+#SBATCH -e logs/minIONQC.%a.txt
+#SBATCH --array=1-4
+
+CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
+sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
+guppy_summary_file=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $4}' $CONFIG)
+minIONQC_OUTPUT=/dcs04/hicks/data/sparthib/casey/minIONQC_outputs/${sample}
+
+mkdir -p $minIONQC_OUTPUT
+
+
+module load conda_R/4.3
+
+Rscript 05_MinIONQC.R -i $guppy_summary_file -o $minIONQC_OUTPUT -p ${SLURM_CPUS_PER_TASK}
+
+
+echo "**** Job ends ****"
+date +"%Y-%m-%d %T"
