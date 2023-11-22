9b76d1ef91f021959b1c0f86e224aa5af3ed4f03
Sowmya Parthiban <sparthi1@jh.edu>
2023-11-22 09:09:02 -0500

    include log dir

--- code/01_fastq_processing/05_fastq_to_bam.sh
+++ code/01_fastq_processing/05_fastq_to_bam.sh
@@ -20,7 +20,7 @@ echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${SLURMD_NODENAME}"
 echo "Task id: ${SLURM_ARRAY_TASK_ID}"
 
-
+LOGS_FOLDER=/users/sparthib/retina_lrs/code/01_fastq_processing/logs/bam_stats
 CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
 INPUT_FOLDER=/dcs04/hicks/data/sparthib/casey/fastqs_post_qc
 REFERENCE_FASTA=/dcs04/hicks/data/sparthib/GENCODE_FASTA.fa
@@ -32,29 +32,46 @@ mkdir -p $SAM_FOLDER
 mkdir -p $BAM_FOLDER
 
 cd ~/minimap2
+
+#remove older sam version 
+rm ${SAM_FOLDER}/${sample}.sam
 ./minimap2 -ax map-ont -N 50 --secondary=no -t ${SLURM_CPUS_PER_TASK} $REFERENCE_FASTA ${INPUT_FOLDER}/${sample}.fastq.gz > ${SAM_FOLDER}/${sample}.sam
 
+
+#remove older bam version
+rm ${BAM_FOLDER}/${sample}_sorted.bam
+rm ${BAM_FOLDER}/${sample}_sorted.bam.bai 
 ml load samtools
+
 samtools view -bS ${SAM_FOLDER}/${sample}.sam -o ${BAM_FOLDER}/${sample}.bam
 samtools sort ${BAM_FOLDER}/${sample}.bam -o ${BAM_FOLDER}/${sample}_sorted.bam
 rm ${BAM_FOLDER}/${sample}.bam
 samtools index ${BAM_FOLDER}/${sample}_sorted.bam ${BAM_FOLDER}/${sample}_sorted.bam.bai 
 
+echo "finished indexing bam"
 #index stats ${sample}_index_stats.txt
-samtools idxstats ${BAM_FOLDER}/${sample}_sorted.bam > ${sample}_index_stats.txt
-#bam stats ${sample}_bam_stats.txt
-echo "stats" > ${sample}_bam_stats.txt
-samtools stats ${BAM_FOLDER}/${sample}_sorted.bam > ${sample}_bam_stats.txt
-echo "flagstat" > ${sample}_bam_stats.txt
-samtools flagstat ${BAM_FOLDER}/${sample}_sorted.bam > ${sample}_bam_stats.txt
+samtools idxstats ${BAM_FOLDER}/${sample}_sorted.bam > ${LOGS_FOLDER}/${sample}_index_stats.txt
 
-echo "depth of coverage" > ${sample}_bam_stats.txt
-samtools depth -a ${BAM_FOLDER}/${sample}_sorted.bam  | awk '{c++;s+=$3}END{print s/c}' > ${sample}_bam_stats.txt
+#bam stats ${sample}_bam.stats for plotting bam stats using plot-bamstats command in samtools
+samtools stats ${BAM_FOLDER}/${sample}_sorted.bam > ${LOGS_FOLDER}/${sample}_bam.stats
 
-echo "breadth of coverage" > ${sample}_bam_stats.txt
-samtools depth -a ${BAM_FOLDER}/${sample}_sorted.bam  | awk '{c++; if($3>0) total+=1}END{print (total/c)*100}'
+echo "finished computing stats for plotting"
 
+echo "flagstat" > ${LOGS_FOLDER}/${sample}_bam_flagstat.txt
+samtools flagstat ${BAM_FOLDER}/${sample}_sorted.bam >> ${LOGS_FOLDER}/${sample}_bam_flagstat.txt
 
+echo "finished computing flagstats: contains percentage mapped reads"
+
+echo "depth of coverage" > ${LOGS_FOLDER}/${sample}_depth_stats.txt
+samtools depth -a ${BAM_FOLDER}/${sample}_sorted.bam  | awk '{c++;s+=$3}END{print s/c}' >> ${LOGS_FOLDER}/${sample}_depth_stats.txt
+
+echo "breadth of coverage" >> ${sample}_depth_stats.txt
+samtools depth -a ${BAM_FOLDER}/${sample}_sorted.bam  | awk '{c++; if($3>0) total+=1}END{print (total/c)*100}' >> ${LOGS_FOLDER}/${sample}_depth_stats.txt
+
+echo "raw depth output" >> ${sample}_depth_stats.txt
+samtools depth -a ${BAM_FOLDER}  >> ${sample}_depth_stats.txt
+
+echo "finished computing depth stats"
 
 echo "**** Job ends ****"
 date +"%Y-%m-%d %T"
