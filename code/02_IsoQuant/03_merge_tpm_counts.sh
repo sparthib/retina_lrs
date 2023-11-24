0ca2018b89b96d4cabc7af8e0b062e0987268451
Sowmya Parthiban <sparthi1@jh.edu>
2023-11-24 12:29:06 -0500

    add header

--- code/02_IsoQuant/03_merge_tpm_counts.sh
+++ code/02_IsoQuant/03_merge_tpm_counts.sh
@@ -27,7 +27,9 @@ IsoQuant_dir=/dcs04/hicks/data/sparthib/casey/IsoQuant_output/${sample}/OUT
 IsoQuant_tpm=$IsoQuant_dir/OUT.transcript_tpm.tsv
 IsoQuant_counts=$IsoQuant_dir/OUT.transcript_counts.tsv
 
-join -t $'\t' -1 1 -2 1 -o 1.1,1.2,2.2 <(sort -k1,1 $IsoQuant_tpm) <(sort -k1,1 $IsoQuant_counts) > $IsoQuant_dir/tpm_counts_data.tsv
+echo "id\ttpm\tcounts" > $IsoQuant_dir/tpm_counts_data.tsv
+join -t $'\t' -1 1 -2 1 -o 1.1,1.2,2.2 <(sort -k1,1 $IsoQuant_tpm) <(sort -k1,1 $IsoQuant_counts) >> $IsoQuant_dir/tpm_counts_data.tsv
+
 
 
 echo "**** Job ends ****"
