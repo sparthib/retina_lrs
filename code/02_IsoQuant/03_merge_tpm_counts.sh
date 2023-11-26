9cf356b0175e11dccca9ffe9e5b96c4f0237ab28
Sowmya Parthiban <sparthi1@jh.edu>
2023-11-25 19:21:27 -0500

    run code in R instead

--- code/02_IsoQuant/03_merge_tpm_counts.sh
+++ code/02_IsoQuant/03_merge_tpm_counts.sh
@@ -20,26 +20,10 @@ echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${SLURMD_NODENAME}"
 echo "Task id: ${SLURM_ARRAY_TASK_ID}"
 
+module load conda_R/4.3
 
-CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
-sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
-IsoQuant_dir=/dcs04/hicks/data/sparthib/casey/IsoQuant_output/${sample}/OUT
-IsoQuant_tpm=$IsoQuant_dir/OUT.transcript_tpm.tsv
-IsoQuant_counts=$IsoQuant_dir/OUT.transcript_counts.tsv
-lengths=/dcs04/hicks/data/sparthib/transcript_lengths_sorted.tsv
-OUTPUT_DIR=/dcs04/hicks/data/sparthib/casey/diff_expression_data/transcript_lengths
+Rscript 03_merge_tpm_counts 
 
-
-echo "id  tpm  counts" > $IsoQuant_dir/tpm_counts_data.tsv
-
-join -t $'\t' -1 1 -2 1 -o 1.1,1.2,2.2 <(sort -k1,1 $IsoQuant_tpm) <(sort -k1,1 $IsoQuant_counts) >> $IsoQuant_dir/tpm_counts_data.tsv
-
-sort -t $'\t' -k1 $IsoQuant_dir/tpm_counts_data.tsv -o $IsoQuant_dir/tpm_counts_data_sorted.tsv
-
-# Sort counts.tsv based on id column
-echo "tx_id tpm counts gene_id nexon  length" > $IsoQuant_dir/tpm_counts_data.tsv
-
-join -t $'\t' -1 1 -2 1 $IsoQuant_dir/tpm_counts_data_sorted.tsv $lengths >> $OUTPUT_DIR/${sample}_transcript_features.tsv
 
 echo "**** Job ends ****"
 date +"%Y-%m-%d %T"
