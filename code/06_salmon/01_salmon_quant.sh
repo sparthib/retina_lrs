4a5373552e09c465ebbd4cb8d729b18f04fc3e41
Sowmya Parthiban <sparthi1@jh.edu>
2023-12-01 13:58:43 -0500

    try salmon

---
+++ code/06_salmon/01_salmon_quant.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH -p shared
+#SBATCH --mem=150G
+#SBATCH --cpus-per-task=20
+#SBATCH --job-name=salmon
+#SBATCH --mail-user=sparthi1@jhu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o logs/salmon.%a.txt
+#SBATCH -e logs/salmon.%a.txt
+#SBATCH --array=1-4
+
+#try running for all chromosomes
+
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+source activate salmon
+
+CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
+sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
+echo "${sample}"
+BAM_FOLDER=/dcs04/hicks/data/sparthib/casey/bams
+REFERENCE_FASTA=/dcs04/hicks/data/sparthib/GENCODE_FASTA.fa 
+
+OUTPUT_FOLDER=/dcs04/hicks/data/sparthib/casey/salmon_output/$sample
+
+salmon quant -t $REFERENCE_FASTA -a aln.bam --libType A \
+-a ${BAM_FOLDER}/${sample}_sorted.bam -o salmon_quant \
+-p ${SLURM_CPUS_PER_TASK} --dumpEq --gcBias --posBias
+
+ 
+conda deactivate
+
+echo "**** Job ends ****"
+jobstats
+date +"%Y-%m-%d %T"
