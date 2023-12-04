910a47e5b281b8c8b56ef76c16558d8b9494f2dc
Sowmya Parthiban <sparthi1@jh.edu>
2023-12-04 10:51:38 -0500

    salmon, transcript level quant

---
+++ code/06_salmon/01b_salmon_quant_transcript_level.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH -p shared
+#SBATCH --mem=150G
+#SBATCH --cpus-per-task=20
+#SBATCH --job-name=salmon
+#SBATCH --mail-user=sparthi1@jhu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o logs/salmon.%a.txt
+#SBATCH -e logs/salmon.%a.txt
+#SBATCH --array=1-4
+
+#try running for all chromosomes
+
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+source activate salmon
+
+CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
+sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
+echo "${sample}"
+BAM_FOLDER=/dcs04/hicks/data/sparthib/casey/bams_2
+REFERENCE_FASTA=/dcs04/hicks/data/sparthib/GENCODE_TRANSCRIPT_SEQs.fa   
+
+rm -r /dcs04/hicks/data/sparthib/casey/salmon_output
+OUTPUT_FOLDER=/dcs04/hicks/data/sparthib/casey/salmon_output/$sample
+mkdir -p $OUTPUT_FOLDER
+
+salmon quant -t $REFERENCE_FASTA --libType A -a $BAM_FOLDER/$sample.bam  -o $OUTPUT_FOLDER --ont --noErrorModel -p ${SLURM_CPUS_PER_TASK}  --gencode
+ 
+conda deactivate
+
+echo "**** Job ends ****"
+
+date +"%Y-%m-%d %T"
