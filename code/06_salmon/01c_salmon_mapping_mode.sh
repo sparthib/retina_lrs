7482df3f3d01ddf7b011dd30796e0794102d9b2e
Sowmya Parthiban <sparthi1@jh.edu>
2023-12-04 14:14:34 -0500

    remove ont flag

--- code/06_salmon/01c_salmon_mapping_mode.sh
+++ code/06_salmon/01c_salmon_mapping_mode.sh
@@ -33,7 +33,7 @@ REFERENCE_FASTA=/dcs04/hicks/data/sparthib/ENSEMBLE_CDNA.fa.gz
 OUTPUT_FOLDER=/dcs04/hicks/data/sparthib/casey/salmon_outputs_transcript_level/$sample
 mkdir -p $OUTPUT_FOLDER
 
-salmon quant -i /dcs04/hicks/data/sparthib/ENSEMBLE_CDNA_salmon_transcript_index --libType A -r $FASTQ/$sample.fastq.gz  -o $OUTPUT_FOLDER --ont --validateMappings -p ${SLURM_CPUS_PER_TASK} 
+salmon quant -i /dcs04/hicks/data/sparthib/ENSEMBLE_CDNA_salmon_transcript_index --libType A -r $FASTQ/$sample.fastq.gz  -o $OUTPUT_FOLDER --validateMappings -p ${SLURM_CPUS_PER_TASK} 
  
 conda deactivate
 
