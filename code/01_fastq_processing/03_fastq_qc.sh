e8bbb042a8d84cec9d0cd270d52172cf6e4377b9
Sowmya Parthiban <sparthi1@jh.edu>
2024-01-02 23:01:44 +0530

    run nanofilt

--- code/01_fastq_processing/03_fastq_qc.sh
+++ code/01_fastq_processing/03_fastq_qc.sh
@@ -7,9 +7,9 @@
 #SBATCH --job-name=fastq_qc
 #SBATCH --mail-user=sparthi1@jhu.edu
 #SBATCH --mail-type=ALL
-#SBATCH -o logs/fastq_qc.%a.txt
-#SBATCH -e logs/fastq_qc.%a.txt
-#SBATCH --array=1-4
+#SBATCH -o logs/03_NanoFilt/fastq_qc.%a.txt
+#SBATCH -e logs/03_NanoFilt/fastq_qc.%a.txt
+#SBATCH --array=1-8
 
 echo "**** Job starts ****"
 date +"%Y-%m-%d %T"
@@ -24,12 +24,11 @@ source activate nanofilt
 
 CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
 sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
-input_fastq=/dcs04/hicks/data/sparthib/casey/fastqs/${sample}.fastq.gz
-fastq_qc_OUTPUT=/dcs04/hicks/data/sparthib/casey/fastqs_post_qc/${sample}.fastq.gz
+echo "$sample"
+input_fastq=/dcs04/hicks/data/sparthib/retina_lrs/01_input_fastqs/${sample}.fastq.gz
+fastq_qc_OUTPUT=/dcs04/hicks/data/sparthib/retina_lrs/03_processed_fastqs/${sample}.fastq.gz
 guppy_summary_file=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $4}' $CONFIG)
 
-
-mkdir -p /dcs04/hicks/data/sparthib/casey/fastqs_post_qc
 
 
 gunzip -c $input_fastq | NanoFilt -q 7 --length 50 -s $guppy_summary_file | gzip > $fastq_qc_OUTPUT
