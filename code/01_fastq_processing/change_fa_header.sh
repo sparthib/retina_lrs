f2f9069d0bc1c9bf622ec9937c7d398133d346e2
Sowmya Parthiban <sparthi1@jh.edu>
2024-01-04 23:35:43 +0530

    run job

---
+++ code/01_fastq_processing/change_fa_header.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH --mem=10G
+#SBATCH --job-name=change_fa_header
+#SBATCH -o logs/change_fa_header.txt
+#SBATCH -e logs/change_fa_header.txt
+
+
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+
+
+ml load python/3.10.13
+python change_fa_header.py
+
+echo "**** Job ends ****"
+date +"%Y-%m-%d %T"
