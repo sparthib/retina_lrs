25fb30ba4a5a75b64a9f54a29898c5c4788078cd
Sowmya Parthiban <sparthi1@jh.edu>
2024-02-11 13:44:28 -0500

    aligned read filter

---
+++ code/03_bam_QC/01_high_quality_bam.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH --mem=50G
+#SBATCH -c 10
+#SBATCH --job-name=highqualbam
+#SBATCH --mail-user=sparthi1@jhu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o logs/MAPQ_filtered/GENCODE_transcriptome/mapq_filter.%a.txt
+#SBATCH -e logs/MAPQ_filtered/GENCODE_transcriptome/mapq_filter.%a.txt
+#SBATCH --array=1-12
+
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+LOGS_FOLDER=/users/sparthib/retina_lrs/code/03_bam_QC/logs/MAPQ_filtered/GENCODE_transcriptome
+CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
+BAM_FOLDER=/dcs04/hicks/data/sparthib/retina_lrs/05_bams/transcriptome/GENCODE
+
+
+
+# https://wikis.utexas.edu/display/CoreNGSTools/Filtering+with+SAMTools#FilteringwithSAMTools-Filteringforhigh-qualityreads
+
+ml load samtools
+
+samtools view -q 30 ${BAM_FOLDER}/${sample}.bam -o ${BAM_FOLDER}/MAPQ_filtered/${sample}.bam
+samtools sort ${BAM_FOLDER}/MAPQ_filtered/${sample}.bam -o ${BAM_FOLDER}/MAPQ_filtered/${sample}_sorted.bam
+samtools index ${BAM_FOLDER}/MAPQ_filtered/${sample}_sorted.bam ${BAM_FOLDER}/MAPQ_filtered/${sample}_sorted.bam.bai
+
+
+# echo "finished indexing bam"
+index stats ${sample}_index_stats.txt
+samtools idxstats ${BAM_FOLDER}/MAPQ_filtered/${sample}_sorted.bam > ${LOGS_FOLDER}/${sample}_index_stats.txt
+
+#bam stats ${sample}_bam.stats for plotting bam stats using plot-bamstats command in samtools
+samtools stats ${BAM_FOLDER}/MAPQ_filtered/${sample}_sorted.bam > ${LOGS_FOLDER}/${sample}_bam.stats
+
+echo "finished computing stats for plotting"
+
+echo "flagstat" > ${LOGS_FOLDER}/${sample}_bam_flagstat.txt
+samtools flagstat ${BAM_FOLDER}/MAPQ_filtered/${sample}_sorted.bam >> ${LOGS_FOLDER}/${sample}_bam_flagstat.txt
+
+echo "finished computing flagstats: contains percentage mapped reads"
+
+echo "depth of coverage" > ${LOGS_FOLDER}/${sample}_depth_stats.txt
+samtools depth -a ${BAM_FOLDER}/MAPQ_filtered/${sample}_sorted.bam  | awk '{c++;s+=$3}END{print s/c}' >> ${LOGS_FOLDER}/${sample}_depth_stats.txt
+
+echo "breadth of coverage" >> ${sample}_depth_stats.txt
+samtools depth -a ${BAM_FOLDER}/MAPQ_filtered/${sample}_sorted.bam  | awk '{c++; if($3>0) total+=1}END{print (total/c)*100}' >> ${LOGS_FOLDER}/${sample}_depth_stats.txt
+
+echo "raw depth output" >> ${sample}_depth_stats.txt
+samtools depth -a ${BAM_FOLDER}/MAPQ_filtered/${sample}_sorted.bam  >> ${sample}_depth_stats.txt
+
+echo "finished computing depth stats"
+
+
+echo "**** Job ends ****"
+date +"%Y-%m-%d %T"
