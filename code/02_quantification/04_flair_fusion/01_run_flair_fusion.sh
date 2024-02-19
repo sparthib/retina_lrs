357d67c8ec94c47f3ecc07433ae4fed3b7f5e9aa
Sowmya Parthiban <sparthi1@jh.edu>
2024-02-19 12:10:02 -0500

    make script for flair fusion

---
+++ code/02_quantification/04_flair_fusion/01_run_flair_fusion.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH --mem=200G
+#SBATCH -c 20
+#SBATCH --job-name=fastq2bam
+#SBATCH --mail-user=sparthi1@jhu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o logs/bam_stats_genome_gencode/minimap_log.%a.txt
+#SBATCH -e logs/bam_stats_genome_gencode/minimap_log.%a.txt
+#SBATCH --array=1
+
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+LOGS_FOLDER=/users/sparthib/retina_lrs/code/02_quantification/04_flair_fusion/logs
+CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
+INPUT_FOLDER=/dcs04/hicks/data/sparthib/retina_lrs/03_processed_fastqs
+sample=$(awk -v Index=${SLURM_ARRAY_TASK_ID} '$1==Index {print $2}' $CONFIG)
+
+### FLAIR-Fusion 
+REFERENCE_GTF=/dcs04/hicks/data/sparthib/references/genome/GENCODE/gencode.v44.chr_patch_hapl_scaff.annotation.gtf
+REFERENCE_FASTA=/dcs04/hicks/data/sparthib/references/genome/GENCODE/GRCh38.p14.genome.fa
+# python ./makeShortAnno.py $REFERENCE_GTF
+
+SHORT_GTF= /users/sparthib/FLAIR-fusion/gencode.v44.chr_patch_hapl_scaff.annotation-short.gtf
+FLAIRPY_PATH=/users/sparthib/flair-2.0.0/flair.py
+#test it on just one fastq file for now 
+python3 ./19-03-2021-fasta-to-fusions-pipe.py -r ${INPUT_FOLDER}/${sample}.fastq.gz \
+-f $FLAIRPY_PATH -g $REFERENCE_FASTA -t $REFERENCE_GTF \
+-a $SHORT_GTF
