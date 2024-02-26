b5b4f4a868d0f96be3f904923f159e86733db8c3
Sowmya Parthiban <sparthi1@jh.edu>
2024-02-25 21:11:05 -0500

    change input to var

--- code/coverage/RCAS.R
+++ code/coverage/RCAS.R
@@ -1,12 +1,12 @@
 library(RCAS)
 
-queryRegions <- importBed("/dcs04/hicks/data/sparthib/retina_lrs/05b_beds/EP1-BRN3B-RO_sorted.bed")
-gff <- importGtf(filePath = "/dcs04/hicks/data/sparthib/references/genome/GENCODE/gencode.v44.chr_patch_hapl_scaff.annotation.gtf")
+# queryRegions <- importBed("/dcs04/hicks/data/sparthib/retina_lrs/05b_beds/EP1-BRN3B-RO_sorted.bed")
+# gff <- importGtf(filePath = "/dcs04/hicks/data/sparthib/references/genome/GENCODE/gencode.v44.chr_patch_hapl_scaff.annotation.gtf")
 
 
-runReport( queryFilePath = 'input.BED',
-           gffFilePath = 'annotation.gtf', 
-           genomeVersion = 'hg19',
+runReport( queryFilePath = "/dcs04/hicks/data/sparthib/retina_lrs/05b_beds/EP1-BRN3B-RO_sorted.bed",
+           gffFilePath = "/dcs04/hicks/data/sparthib/references/genome/GENCODE/gencode.v44.chr_patch_hapl_scaff.annotation.gtf", 
+           genomeVersion = 'hg38',
            goAnalysis=FALSE,
            outDir = "/users/sparthib/retina_lrs/processed_data/coverage/RCAS")
 
