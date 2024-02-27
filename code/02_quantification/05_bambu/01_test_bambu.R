418c9867b0ef433886afffcfcd25a69fc4969f66
Sowmya Parthiban <sparthi1@jh.edu>
2024-02-27 10:45:49 -0500

    run discovery only mode on bambu

--- code/02_quantification/05_bambu/01_test_bambu.R
+++ code/02_quantification/05_bambu/01_test_bambu.R
@@ -1,22 +1,55 @@
 library(bambu)
-
+library(BiocFileCache)
 
 sample  <- commandArgs(trailingOnly = TRUE)
 test.bam <- paste0("/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE/MAPQ_FILTERED/", sample, "_sorted.bam")
 fa.file <- "/dcs04/hicks/data/sparthib/references/genome/GENCODE/GRCh38.p14.genome.fa"
 gtf.file <- "/dcs04/hicks/data/sparthib/references/genome/GENCODE/gencode.v44.chr_patch_hapl_scaff.annotation.gtf"
 
-se_output_dir <- paste0("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/", sample)
+se_output_dir <- "/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/EP1-BRN3B-RO"
 
 
 bambuAnnotations <- prepareAnnotations(gtf.file)
-se <- bambu(reads = test.bam, 
-            annotations = bambuAnnotations,
-            genome = fa.file,
-            ncore = 17,
-            rcOutDir = se_output_dir) 
+# se <- bambu(reads = test.bam, 
+#             annotations = bambuAnnotations,
+#             genome = fa.file,
+#             ncore = 17,
+#             rcOutDir = se_output_dir) 
 
 
-# quant = FALSE ## transcript discovery only
-# discovery = FALSE ## quantification only 
+
+bfc <- BiocFileCache(se_output_dir, ask = FALSE)
+info <- bfcinfo(bfc)
+
+
+# se <- bambu(reads = c("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/EP1-BRN3B-RO/32836422b0c117_32836422b0c117.rds"),
+#             annotations = bambuAnnotations, genome = fa.file)
+# Reads should either be: a vector of paths to .bam files, a vector of paths to Bambu RCfile .rds files, 
+# or a list of loaded Bambu RCfiles
+# pdf("/users/sparthib/retina_lrs/plots/bambu/ENSG00000116670.pdf")
+# p <- plotBambu(se, type = "annotation", gene_id = "ENSG00000116670.16")
+# print(p)
+# dev.off()
+
+# writeBambuOutput(se, 
+#                  path = "/users/sparthib/retina_lrs/processed_data/bambu",
+#                  prefix = "EP1-BRN3B-RO")
+
+se.discoveryOnly <- bambu(reads = test.bam,
+                          annotations = bambuAnnotations,
+                          genome = fa.file,
+                          quant = FALSE,
+                          NDR = 1,
+                          ncore = 17,
+                          rcOutDir = se_output_dir)
+                          
+                          
+writeBambuOutput(se.discoveryOnly, 
+                 path = "/users/sparthib/retina_lrs/processed_data/bambu",
+                 prefix = "EP1-BRN3B-RO_discovery_only")
+
+
+
+
+
 
