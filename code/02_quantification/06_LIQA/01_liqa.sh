90a39065e365ae379dea696b928924bff8d22c80
Sowmya Parthiban <sparthi1@jh.edu>
2024-02-28 13:49:23 -0500

    rerun on example file

--- code/02_quantification/06_LIQA/01_liqa.sh
+++ code/02_quantification/06_LIQA/01_liqa.sh
@@ -27,22 +27,24 @@ CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
 sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
 echo "${sample}"
 BAM_FOLDER=/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE/MAPQ_FILTERED
-REFERENCE_GTF=/dcs04/hicks/data/sparthib/references/genome/GENCODE/gencode.v44.chr_patch_hapl_scaff.annotation.gtf
+# REFERENCE_GTF=/dcs04/hicks/data/sparthib/references/genome/GENCODE/gencode.v44.chr_patch_hapl_scaff.annotation.gtf
+REFERENCE_GTF=/users/sparthib/LIQA_example/sample.gtf
 STEP1_OUTPUT=/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/LIQA/LIQA_STEP_1
 STEP2_OUTPUT=/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/LIQA/step2_outputs
 STEP_3_OUTPUT=/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/LIQA/step3_outputs
 
-mkdir -p $STEP1_OUTPUT
+
+rm $STEP1_OUTPUT/step1.refgene
 touch $STEP1_OUTPUT/step1.refgene
-mkdir -p $STEP2_OUTPUT
-mkdir -p $STEP_3_OUTPUT
+# mkdir -p $STEP2_OUTPUT
+# mkdir -p $STEP_3_OUTPUT
 
 ##STEP 1 - preprocess GTF
 liqa -task refgene -ref $REFERENCE_GTF -format gtf -out $STEP1_REFGENE/step1.refgene
 
 ##STEP 2 - quantify isoform expression 
 
-liqa -task quantify -refgene $STEP1_OUTPUT/step1.refgene -bam ${BAM_FOLDER}/${sample}_sorted.bam -out $STEP2_OUTPUT/$sample -max_distance 20 -f_weight 1
+# liqa -task quantify -refgene $STEP1_OUTPUT/step1.refgene -bam ${BAM_FOLDER}/${sample}_sorted.bam -out $STEP2_OUTPUT/$sample -max_distance 20 -f_weight 1
 
 conda deactivate
 
