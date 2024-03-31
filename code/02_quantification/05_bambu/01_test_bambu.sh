2e12a1d5b6203a389385da9c53ecf3014c05d234
Sowmya Parthiban <sparthi1@jh.edu>
2024-03-31 11:33:34 -0400

    remove array

--- code/02_quantification/05_bambu/01_test_bambu.sh
+++ code/02_quantification/05_bambu/01_test_bambu.sh
@@ -8,7 +8,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH -o logs/test_bambu.txt
 #SBATCH -e logs/test_bambu.txt
-#SBATCH --array=11
+
 
 echo "**** Job starts ****"
 date +"%Y-%m-%d %T"
@@ -17,11 +17,10 @@ echo "User: ${USER}"
 echo "Job id: ${SLURM_JOB_ID}"
 echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${SLURMD_NODENAME}"
-echo "Task id: ${SLURM_ARRAY_TASK_ID}"
 
 CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
 # sample=$(sed -n ${SLURM_ARRAY_TASK_ID}p $CONFIG | awk '{print $2}')
-mkdir -p /dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/$sample
+# mkdir -p /dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/$sample
 
 module load conda_R/4.3.x
 Rscript 01_test_bambu.R 
