65b7bbb705166e1bd234d3488fbc822b27d62697
Sowmya Parthiban <sparthi1@jh.edu>
2024-04-07 08:25:16 -0400

    split by chr and run quant mode

--- code/02_quantification/05_bambu/01_test_bambu.R
+++ code/02_quantification/05_bambu/01_test_bambu.R
@@ -8,24 +8,27 @@ library(dplyr)
 # bambuAnnotations <- prepareAnnotations(gtf.file)
 # saveRDS(bambuAnnotations, "/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/annotations.rds")
 
+sample <- commandArgs(trailingOnly = TRUE)
+
 annotation <- readRDS("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/annotations.rds")
 
 bam_dir <- "/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/"
+bam_dir <- paste0(bam_dir, sample, "_chromosome_level/")
 fa.file <- "/dcs04/hicks/data/sparthib/references/genome/GENCODE/GRCh38.p14.genome.fa"
-se_output_dir <- paste0("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/", sample, "/")
 
-samples <- c("EP1-BRN3B-RO", "H9-BRN3B-RO", "hRGC", "DG-WT-hRGC",
-                          "H9-CRX_ROs_D45", "EP1-WT_ROs_D45", "YZ-3T_hRGC",
-                          "YZ-15T_hRGC", "H9-FT_1","H9-FT_2", "H9-hRGC_2")
 
-for (sample in samples){ 
+
+
+for (chr in c(1:22, "X", "Y", "M")){ 
+  se_output_dir <- paste0("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/", sample, "/")
   print(sample)
-  novelOnly <- bambu(reads = paste0(bam_dir, sample, "_sorted.bam"),
+  print(chr)
+  se_quant_chr_sample <- bambu(reads = paste0(bam_dir, sample, "_", chr, ".bam"),
                      annotations = annotation,
                      genome = fa.file,
                      quant = FALSE, NDR = 1)
   
-  writeBambuOutput(novelOnly, 
+  writeBambuOutput(se_quant_chr_sample, 
                    path = se_output_dir,
                    prefix = sample)
   }
--- code/02_quantification/05_bambu/01_test_bambu.sh
+++ code/02_quantification/05_bambu/01_test_bambu.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #SBATCH -p shared
-#SBATCH --mem=250G
+#SBATCH --mem=100G
 #SBATCH -c 20
 #SBATCH --job-name=test_bambu
 #SBATCH --mail-user=sparthi1@jhu.edu
 #SBATCH --mail-type=ALL
-#SBATCH -o logs/bambu_novel.txt
-#SBATCH -e logs/bambu_novel.txt
+#SBATCH -o logs/bambu_quant.txt
+#SBATCH -e logs/bambu_quant.txt
 #SBATCH --time=7-00:00:00
 
 echo "**** Job starts ****"
@@ -19,11 +19,10 @@ echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${SLURMD_NODENAME}"
 
 CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
-sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
-# mkdir /dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/$sample
+sample=sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
 
 module load conda_R/4.3.x
-Rscript 01_test_bambu.R 
+Rscript 01_test_bambu.R $sample
 
 echo "**** Job ends ****"
 date
