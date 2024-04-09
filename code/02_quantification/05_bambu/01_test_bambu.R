1e4cd17fe790b04d1c7b547120b84ecf583e161d
Sowmya Parthiban <sparthi1@jh.edu>
2024-04-09 12:44:44 -0400

    produce extended annotation only

--- code/02_quantification/05_bambu/01_test_bambu.R
+++ code/02_quantification/05_bambu/01_test_bambu.R
@@ -18,18 +18,18 @@ fa.file <- "/dcs04/hicks/data/sparthib/references/genome/GENCODE/GRCh38.p14.geno
 
 for (chr in c(1:22, "X", "Y", "M")){ 
   # ‘/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/EP1-BRN3B-RO_chromosome_level/EP1-BRN3B-RO_1.bam’
-  se_output_dir <- paste0("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/", sample, "/", chr, "/")
+  output_dir <- paste0("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/rc_output", sample, "/", "chr",chr, "/")
   print(sample)
   print(chr)
-  se_quant_chr_sample <- bambu(reads = paste0(bam_dir, sample, "_chr",chr, ".bam"),
+  se_read_class_chr_sample <- bambu(reads = paste0(bam_dir, sample, "_chr",chr, ".bam"),
                      annotations = annotation,
                      genome = fa.file,
-                     quant = TRUE)
-  
-  writeBambuOutput(se_quant_chr_sample, 
-                   path = se_output_dir,
-                   prefix = sample)
-  }
+                     discovery = FALSE, quant = FALSE, rcOutDir = output_dir)
+}
+  # writeBambuOutput(se_quant_chr_sample, 
+  #                  path = se_output_dir,
+  #                  prefix = sample)
+  # }
 
 sessioninfo::session_info()
                           
