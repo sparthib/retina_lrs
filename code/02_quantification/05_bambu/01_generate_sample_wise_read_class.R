55eabca605e40a775c44925e20764a2f52578117
Sowmya Parthiban <sparthi1@jh.edu>
2024-04-16 09:38:32 -0400

    remove chr level input dir

--- code/02_quantification/05_bambu/01_generate_sample_wise_read_class.R
+++ code/02_quantification/05_bambu/01_generate_sample_wise_read_class.R
@@ -12,7 +12,6 @@ sample <- commandArgs(trailingOnly = TRUE)
 
 annotation <- readRDS("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/annotations.rds")
 bam_dir <- "/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/"
-bam_dir <- paste0(bam_dir, sample, "_chromosome_level/")
 fa.file <- "/dcs04/hicks/data/sparthib/references/genome/GENCODE/GRCh38.p14.genome.fa"
 
 
