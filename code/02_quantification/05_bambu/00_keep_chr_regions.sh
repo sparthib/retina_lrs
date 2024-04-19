24e4dfe39f2baf756367c98b6ab51d5a5c82b6e6
Sowmya Parthiban <sparthi1@jh.edu>
2024-04-19 10:26:25 -0400

    merge chr regions

---
+++ code/02_quantification/05_bambu/00_keep_chr_regions.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH --mem=50G
+#SBATCH -c 20
+#SBATCH --job-name=test_bambu
+#SBATCH --mail-user=sparthi1@jhu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o logs/merge_chr.%a.txt
+#SBATCH -e logs/merge_chr.%a.txt
+#SBATCH --time=7-00:00:00
+#SBATCH --array=3-12
+
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "Array ID: ${SLURM_ARRAY_TASK_ID}"
+
+
+CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
+sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
+bam_dir=/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/${sample}_chromosome_level/
+
+ml load samtools 
+
+cd $bam_dir
+echo "**** Processing sample $sample ****"
+
+
+samtools merge $bam_dir/${sample}.bam *.bam 
+bam_dir <- paste0(bam_dir, sample, "_chromosome_level/", sample, ".bam")
+
+
+echo "**** Job ends ****"
+date
