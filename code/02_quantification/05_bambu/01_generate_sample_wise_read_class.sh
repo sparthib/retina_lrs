32a9f21dd58c8c7ea7f29043c82e027ae910db2d
Sowmya Parthiban <sparthi1@jh.edu>
2024-04-21 16:23:30 -0400

    retry for sample 2

--- code/02_quantification/05_bambu/01_generate_sample_wise_read_class.sh
+++ code/02_quantification/05_bambu/01_generate_sample_wise_read_class.sh
@@ -9,7 +9,7 @@
 #SBATCH -o logs/bambu_quant.%a.txt
 #SBATCH -e logs/bambu_quant.%a.txt
 #SBATCH --time=7-00:00:00
-#SBATCH --array=1-2
+#SBATCH --array=2
 
 echo "**** Job starts ****"
 date +"%Y-%m-%d %T"
