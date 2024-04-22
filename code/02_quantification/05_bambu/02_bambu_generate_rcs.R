363f57c58f4c174f32e07cd8aafc7f95c1877760
Sowmya Parthiban <sparthi1@jh.edu>
2024-04-21 22:39:02 -0400

    change rds path and extended annotation gtf argument

--- code/02_quantification/05_bambu/02_bambu_generate_rcs.R
+++ code/02_quantification/05_bambu/02_bambu_generate_rcs.R
@@ -9,18 +9,21 @@ annotation <- readRDS("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/b
 bam_dir <- "/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/"
 fa.file <- "/dcs04/hicks/data/sparthib/references/genome/GENCODE/GRCh38.p14.genome.fa"
 
-root_dir <- "/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/rc_output/"
+root_dir <- "/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/rc_output"
 files <- list.files(root_dir, recursive = TRUE, full.names = TRUE)
 rds_files <- files[grep("\\.rds$", files)]
 
-
 extendedAnnotations <- bambu(reads = rds_files,
-                             annotations = annotation, 
-                             genome = fa.file, 
-                             discovery = TRUE, 
-                             quant = FALSE)
-writeToGTF(extendedAnnotations, "/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/multisample_output.gtf")
+                             annotations = annotation,
+                             genome = fa.file)
 
+writeToGTF(extendedAnnotations, "/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/multisample_output_apr_21.gtf")
 
 sessioninfo::session_info()
+# library(rtracklayer)
+# discovery_only <- readGFF("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/multisample_output.gtf")
+# 
+# plotBambu(discovery_only, type = "heatmap")
+# 
+
 
