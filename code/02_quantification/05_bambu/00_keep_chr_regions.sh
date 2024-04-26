778b3b2baf56641707af5dd54a31f6409698ca3a
Sowmya Parthiban <sparthi1@jh.edu>
2024-04-26 11:45:26 -0400

    merge chr files only for new samples

--- code/02_quantification/05_bambu/00_keep_chr_regions.sh
+++ code/02_quantification/05_bambu/00_keep_chr_regions.sh
@@ -9,7 +9,7 @@
 #SBATCH -o logs/merge_chr.%a.txt
 #SBATCH -e logs/merge_chr.%a.txt
 #SBATCH --time=7-00:00:00
-#SBATCH --array=3-12
+#SBATCH --array=13-15
 
 echo "**** Job starts ****"
 date +"%Y-%m-%d %T"
