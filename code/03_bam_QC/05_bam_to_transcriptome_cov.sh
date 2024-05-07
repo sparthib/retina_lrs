e3b7143aa8ac045f7d4dd52ec0bd4c4f0ebffbc7
Sowmya Parthiban <sparthi1@jh.edu>
2024-05-07 12:47:28 -0400

    try bedtools for transcriptome cov

---
+++ code/03_bam_QC/05_bam_to_transcriptome_cov.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH --mem=40G
+#SBATCH -c 20
+#SBATCH --job-name=genomecov
+#SBATCH --mail-user=sparthi1@jhu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o logs/transcriptome_cov/transcriptome_cov.%a.txt
+#SBATCH -e logs/transcriptome_cov/transcriptome_cov.%a.txt
+#SBATCH --array=1-15
+
+
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+echo "****"
+
+
+
+CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
+sample=$(awk -v Index=${SLURM_ARRAY_TASK_ID} '$1==Index {print $2}' $CONFIG)
+echo $sample
+
+transcriptome_input_bam=/dcs04/hicks/data/sparthib/retina_lrs/05_bams/transcriptome/GENCODE/sorted/primary_over_30/${sample}_sorted.bam
+transcriptome_coverage=/dcs04/hicks/data/sparthib/retina_lrs/05c_coverage/transcriptome
+
+#use bedtools to convert bam to transcritpome coverage files 
+bedtools genomecov -bed12 -ibam $transcriptome_input_bam  > $transcriptome_coverage/${sample}.coverage.txt
+
+echo "**** Job ends ****"
+date +"%Y-%m-%d %T"
+
