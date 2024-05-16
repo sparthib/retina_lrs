85d3ebadc3d31947c5b054b8428c1407ba44a4c6
Sowmya Parthiban <sparthi1@jh.edu>
2024-05-16 09:19:41 -0400

    try single cell wf

---
+++ code_single_cell/02_ont-wf-single-cell /01_run_ont_nf.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH --mem=25G
+#SBATCH --job-name=concat_fqs
+#SBATCH -c 2
+#SBATCH --mail-user=sparthi1@jhu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o logs/concat/concat_fqs.%a.txt
+#SBATCH -e logs/concat/concat_fqs.%a.txt
+#SBATCH --array=1
+
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+
+ml load nextflow
+ml load singularity
+
+CONFIG=/users/sparthib/retina_lrs/raw_data/single_cell.config
+INPUT_FOLDER=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $3}' $CONFIG)
+sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
+echo $sample
+FASTQ='/dcs04/hicks/data/sparthib/retina_single_cell_lrs/01_input_fastqs/${sample}.fastq.gz'
+output_folder='/dcs04/hicks/data/sparthib/retina_single_cell_lrs/02_ont_wf_single_cell_output/'
+
+
+nextflow run epi2me-labs/wf-single-cell \
+    --fastq $FASTQ \
+    --expected_cells 100 \
+    --kit_name '3prime' \
+    --kit_version 'v3' \
+    --ref_genome_dir '/dcs04/hicks/data/sparthib/references/genome/GENCODE/' \
+    --profile singularity
+    --out_dir $output_folder
+
+echo "**** Job ends ****"
+date +"%Y-%m-%d %T"
