320f3f9aef3e1cae76387c96f54f44d22a43d7d0
Sowmya Parthiban <sparthi1@jh.edu>
2024-05-17 12:00:59 -0400

    comment out code

--- code_single_cell/01_fastq_processing/02_fastq_qc.sh
+++ code_single_cell/01_fastq_processing/02_fastq_qc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #SBATCH -p shared
+#SBATCH --nodes=1
 #SBATCH --mem=40G
 #SBATCH -c 10
 #SBATCH --job-name=fastq_qc
@@ -19,21 +20,21 @@ echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${SLURMD_NODENAME}"
 echo "Task id: ${SLURM_ARRAY_TASK_ID}"
 
-source activate nanofilt 
+# source activate nanofilt 
 echo "nanofilt on"
-
-CONFIG=/users/sparthib/retina_lrs/raw_data/single_cell.config
-sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
-path=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $3}' $CONFIG)
-seq_sum=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $4}' $CONFIG)
-echo $sample
-
-input_fastq=/dcs04/hicks/data/sparthib/retina_single_cell_lrs/01_input_fastqs/${sample}.fastq.gz
-fastq_qc_output=/dcs04/hicks/data/sparthib/retina_single_cell_lrs/02_nanofilt_processed/${sample}.fastq.gz
-guppy_summary_file=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $4}' $CONFIG)
-
-echo "processing input fastq"
-gunzip -c $input_fastq | NanoFilt -q 7 --length 50 -s ${path}${seq_sum} | gzip > $fastq_qc_output
+# 
+# CONFIG=/users/sparthib/retina_lrs/raw_data/single_cell.config
+# sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
+# path=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $3}' $CONFIG)
+# seq_sum=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $4}' $CONFIG)
+# echo $sample
+# 
+# input_fastq=/dcs04/hicks/data/sparthib/retina_single_cell_lrs/01_input_fastqs/${sample}.fastq.gz
+# fastq_qc_output=/dcs04/hicks/data/sparthib/retina_single_cell_lrs/02_nanofilt_processed/${sample}.fastq.gz
+# guppy_summary_file=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $4}' $CONFIG)
+# 
+# echo "processing input fastq"
+# gunzip -c $input_fastq | NanoFilt -q 7 --length 50 -s ${path}${seq_sum} | gzip > $fastq_qc_output
 
 echo "**** Job ends ****"
 date +"%Y-%m-%d %T"
