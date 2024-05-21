0283be5777eabaa78c59bae65916078f706bf668
Sowmya Parthiban <sparthi1@jh.edu>
2024-05-21 19:52:23 -0400

    subset by sample type

--- code/02_quantification/04_flair2/01b_split_flair_corrected_bed_by_chr.sh
+++ code/02_quantification/04_flair2/01b_split_flair_corrected_bed_by_chr.sh
@@ -19,17 +19,51 @@ echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${SLURMD_NODENAME}"
 echo "****"
 
+$sample_wise_bed_dir=/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flair2/correction_output/
 
+#H9 FT vs HRGC
+for file in $sample_wise_bed_dir/H9-FT_1_all_corrected.bed $sample_wise_bed_dir/H9-FT_2_all_corrected.bed $sample_wise_bed_dir/H9-hRGC_2_all_corrected.bed $sample_wise_bed_dir/H9-hRGC_1_all_corrected.bed;
+do 
+	cat "$file" >> $sample_wise_bed_dir/all_H9_FT_RGC_samples_corrected.bed
+done
 
-input=/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flair2/correction_output/all_samples_corrected.bed
-output_dir=/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flair2/correction_output/all_sample_chr_split
+input=/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flair2/correction_output/all_H9_FT_RGC_samples_corrected.bed
+output_dir=/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flair2/correction_output/all_H9_FT_RGC_sample_chr_split
 mkdir -p $output_dir
 
-for chr in `cut -f 1 $input | sort | uniq`;
+for chr in {1..22} X Y M;
 do
 	echo $chr
 	grep -w $chr $input > $output_dir/$chr.bed
 done
 
+
+
+#for files that have RO in their name (i.e. RO_1.bed, RO_2.bed, etc.)
+for file in $sample_wise_bed_dir/*RO*.bed; 
+do
+	cat "$file" >> $sample_wise_bed_dir/all_RO_samples_corrected.bed
+done
+
+input=/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flair2/correction_output/all_RO_samples_corrected.bed
+output_dir=/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flair2/correction_output/all_RO_sample_chr_split
+mkdir -p $output_dir
+
+for chr in {1..22} X Y M;
+do
+	echo $chr
+	grep -w $chr $input > $output_dir/$chr.bed
+done
+
+
+
+
+
+
+
+
 echo "**** Job ends ****"
 date +"%Y-%m-%d %T"
+
+
+
