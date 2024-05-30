17a99d0d71f9f30883f1ee199e76e4a26e0fd859
Sowmya Parthiban <sparthi1@jh.edu>
2024-05-30 19:46:43 -0400

    remove chr related commands

--- code/03_bam_QC/01b_high_quality_bam_genome.sh
+++ code/03_bam_QC/01b_high_quality_bam_genome.sh
@@ -32,15 +32,10 @@ mkdir -p $output_dir
 bam=$input_dir/${sample}_sorted.bam
 
 ml load samtools 
-q
 
 samtools view -b -q 30 -F 0x800 -h $bam > $output_dir/${sample}_primary_over_30.bam
 samtools sort $output_dir/${sample}_primary_over_30.bam -o $output_dir/${sample}_primary_over_30_sorted.bam
 samtools index $output_dir/${sample}_primary_over_30_sorted.bam $output_dir/${sample}_primary_over_30_sorted.bam.bai
-samtools view -h -b $output_dir/${sample}_primary_over_30_sorted.bam chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 \ 
-chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY chrM -o $output_dir/${sample}_chr_only.bam
-samtools sort $output_dir/${sample}_chr_only.bam -o $output_dir/${sample}_chr_only_sorted.bam
-samtools index $output_dir/${sample}_chr_only_sorted.bam $output_dir/${sample}_chr_only_sorted.bam.bai
 
 # # echo "finished indexing bam"
 # samtools idxstats $primary_over_30/${sample}_sorted.bam > ${LOGS_FOLDER}/primary_over_30_${sample}_index_stats.txt
