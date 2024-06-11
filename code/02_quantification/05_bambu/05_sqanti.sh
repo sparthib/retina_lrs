bf8d9c32bcf4866e368765958c6c22fd5669e0e6
Sowmya Parthiban <sparthi1@jh.edu>
2024-06-11 14:35:14 -0400

    change -o arg

--- code/02_quantification/05_bambu/05_sqanti.sh
+++ code/02_quantification/05_bambu/05_sqanti.sh
@@ -21,10 +21,6 @@ echo "Node name: ${SLURMD_NODENAME}"
 echo "****"
 
 
-CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
-sample=$(awk -v Index=${SLURM_ARRAY_TASK_ID} '$1==Index {print $2}' $CONFIG)
-echo $sample
-
 #reference genome 
 REFERENCE_GENOME_FASTA=/dcs04/hicks/data/sparthib/references/genome/GENCODE/GRCh38.p14.genome.fa
 REFERENCE_GTF=/dcs04/hicks/data/sparthib/references/genome/GENCODE/gencode.v44.chr_patch_hapl_scaff.annotation.gtf
@@ -45,12 +41,12 @@ source activate /users/sparthib/.conda/envs/SQANTI3
 SQANTI_DIR=~/SQANTI3-5.2.1
 
 python $SQANTI_DIR/sqanti3_qc.py  ${ROs_GTF} ${REFERENCE_GTF} ${REFERENCE_GENOME_FASTA} \
-    --skipORF -o $sample -d $RO_OUTPUT_DIR/sqanti3_qc --saturation \
+    --skipORF -o ROs -d $RO_OUTPUT_DIR/sqanti3_qc --saturation \
     -t $SLURM_CPUS_PER_TASK --report skip --isoform_hits 
      ##positional arguments
      
 python $SQANTI_DIR/sqanti3_qc.py  ${FT_vs_RGC_GTF} ${REFERENCE_GTF} ${REFERENCE_GENOME_FASTA} \
-    --skipORF -o $sample -d $FT_vs_RGC_OUTPUT_DIR/sqanti3_qc --saturation \
+    --skipORF -o FT_vs_RGC -d $FT_vs_RGC_OUTPUT_DIR/sqanti3_qc --saturation \
     -t $SLURM_CPUS_PER_TASK --report skip --isoform_hits
 
 
