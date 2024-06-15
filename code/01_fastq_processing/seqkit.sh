a32aae74c2a0f6119cf80699d7d7272b82bcf838
Sowmya Parthiban <sparthi1@jh.edu>
2024-06-15 17:57:46 -0400

    change args

--- code/01_fastq_processing/seqkit.sh
+++ code/01_fastq_processing/seqkit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH -p shared
-#SBATCH --mem=25G
+#SBATCH --mem=80G
 #SBATCH --job-name=seqkit
-#SBATCH -c 5
+#SBATCH -c 10
 #SBATCH --mail-user=sparthi1@jhu.edu
 #SBATCH --mail-type=ALL
 #SBATCH -o logs/seqkit/seqkit.%a.txt
@@ -25,11 +25,13 @@ sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
 echo $sample
 INPUT_FOLDER=/dcs04/hicks/data/sparthib/retina_lrs/01_input_fastqs
 OUTPUT_FOLDER=/dcs04/hicks/data/sparthib/retina_lrs/01a_seqkit_processed
-$INPUT_FOLDER/${sample}.fastq.gz
 
-seqkit rmdup --dup-num-file $OUTPUT_FOLDER/$sample.dup_ids.txt  \
---dup-seqs-file $OUTPUT_FOLDER/$sample.dup_seqs.txt \
+
+zcat $INPUT_FOLDER/${sample}.fastq.gz |  seqkit rmdup --dup-num-file $OUTPUT_FOLDER/$sample.dup_ids.txt  \
+--dup-seqs-file $OUTPUT_FOLDER/$sample.duplicated.fa.gz \
 -o $OUTPUT_FOLDER/$sample.cleaned.fastq.gz \
+
+conda deactivate 
 
 echo "**** Job ends ****"
 date +"%Y-%m-%d %T"
