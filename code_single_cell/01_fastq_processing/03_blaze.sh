d19a32443f76456bb1f725ba4a9cccba6939cf22
Sowmya Parthiban <sparthi1@jh.edu>
2024-06-20 15:41:44 -0400

    change task ID

--- code_single_cell/01_fastq_processing/03_blaze.sh
+++ code_single_cell/01_fastq_processing/03_blaze.sh
@@ -9,7 +9,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH -o logs/blaze/raw_high_sensitivity.%a.txt
 #SBATCH -e logs/blaze/raw_high_sensitivity.%a.txt
-#SBATCH --array=5-12
+#SBATCH --array=1-12
 #SBATCH --time=7-00:00:00
 
 echo "**** Job starts ****"
