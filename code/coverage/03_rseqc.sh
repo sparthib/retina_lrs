cecb691f9f320a24b1ed6c798e3919e758741621
Sowmya Parthiban <sparthi1@jh.edu>
2024-06-26 13:52:16 -0400

    rename coverage plotting scripts

---
+++ code/coverage/03_rseqc.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH --mem=30G
+#SBATCH -c 10
+#SBATCH --job-name=gene_body_cov
+#SBATCH --mail-user=sparthi1@jhu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o logs/plot_gene_body_cov_transcript_length.txt
+#SBATCH -e logs/plot_gene_body_cov_transcript_length.txt
+
+
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+# echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+echo "****"
+
+ref_gene_model=/dcs04/hicks/data/sparthib/references/rseqc_gencode_44_comp.bed
+
+ml load rseqc/3.0.1
+
+geneBody_coverage.py -i $LONG_BAM_FILE -r $ref_gene_model -o $long_output/long_
+
+echo "**** Job ends ****"
+date +"%Y-%m-%d %T"
