f574edaf4070ff8de84b23291d052231049c3cc3
Sowmya Parthiban <sparthi1@jh.edu>
2024-06-26 09:33:31 -0400

    flames test run

---
+++ code_single_cell/01_fastq_processing/flames_test.R
@@ -0,0 +1,49 @@
+library("FLAMES")
+library("sessioninfo")
+
+## blaze FASTQ files 
+
+
+fastq <- file.path("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/03_blaze_processed/raw/high_sensitivity/10x_D200-EP1-1_B2_matched_reads.fastq.gz")
+genome_fa <- file.path("/dcs04/hicks/data/sparthib/references/genome/GENCODE/GRCh38.p14.genome.fa")
+annot <- file.path("/dcs04/hicks/data/sparthib/references/genome/GENCODE/gencode.v44.chr_patch_hapl_scaff.annotation-short.gtf")
+minimap_path <- file.path("/users/sparthib/minimap2")
+
+outdir <- file.path("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output")
+config_file <- FLAMES::create_config(outdir, type = "sc_3end", do_barcode_demultiplex = FALSE,
+                                     min_sup_cnt = 5)
+
+#run minimap2
+
+if (!any(is.na(sys_which(c("minimap2", "k8"))))) {
+  # sce <- sc_long_pipeline(
+  #   annotation = annot, fastq = fastq, genome_fa = genome_fa,
+  #   outdir = outdir, config_file = config_file, expect_cell_number = 63)
+  
+  config <- jsonlite::fromJSON(config_file)
+  
+
+}
+
+genome_bam <- rownames(minimap2_align(
+  config = config, fa_file = genome_fa, fq_in = fastq, annot = annot,
+  outdir = outdir
+))
+print("completed genome alignment")
+
+find_isoform(
+  annotation = annot, genome_fa = genome_fa,
+  genome_bam = genome_bam, outdir = outdir, config = config
+)
+print("completed isoform identification")
+minimap2_realign(
+  config = config, fq_in = fastq,
+  outdir = outdir
+)
+print("created minimap2 realigned bam")
+quantify_transcript(annotation = annot, outdir = outdir, config = config)
+print("completed transcript quantification")
+sce <- create_sce_from_dir(outdir = outdir, annotation = annot)
+print("created SingleCellExperiment object")
+
+sessionInfo()
---
+++ code_single_cell/01_fastq_processing/flames_test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -p shared
+#SBATCH --mem=50G
+#SBATCH --job-name=flames
+#SBATCH -c 10
+#SBATCH --mail-user=sparthi1@jhu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o logs/flames/10x_D200-EP1-1_B2.txt
+#SBATCH -e logs/flames/10x_D200-EP1-1_B2.txt
+
+
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+
+ml load conda_R/4.4.x
+Rscript flames_test.R
+
+echo "**** Job ends ****"
+date +"%Y-%m-%d %T"
