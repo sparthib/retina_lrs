67c0c8c8f9cca69a79e48bf1eb858220b6e97541
Sowmya Parthiban <sparthi1@jh.edu>
2024-07-01 15:45:31 -0400

    replace `awk` with `cut`

--- code/coverage/00_extract_read_ids.sh
+++ code/coverage/00_extract_read_ids.sh
@@ -10,6 +10,15 @@
 #SBATCH -e logs/extract_read_ids.%a.txt
 #SBATCH --array=1-15
 
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "Array index: ${SLURM_ARRAY_TASK_ID}"
+
 
 CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
 BAM_FOLDER=/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/primary_over_30_chr_only/
@@ -18,4 +27,4 @@ echo "$sample"
 
 ml load samtools 
 # Extract read ids from bam files
-samtools view $BAM_FOLDER/${sample}_primary_over_30_chr_only_sorted.bam | awk '{if($1 ~ /^@/ || $1 ~ /NH:i:1/) print $1}' > $BAM_FOLDER/${sample}_read_ids.txt
+samtools view $BAM_FOLDER/${sample}_primary_over_30_chr_only_sorted.bam | cut -f1 > $BAM_FOLDER/${sample}_read_ids.txt
