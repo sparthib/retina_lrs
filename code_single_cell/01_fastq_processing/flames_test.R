6dbd39a07271eff62f0cbbfcfc3cbd1b43c1784c
Sowmya Parthiban <sparthi1@jh.edu>
2024-07-01 19:24:52 -0400

    try alternate pipeline

--- code_single_cell/01_fastq_processing/flames_test.R
+++ code_single_cell/01_fastq_processing/flames_test.R
@@ -3,7 +3,6 @@ library("sessioninfo")
 
 ## blaze FASTQ files 
 
-
 fastq <- file.path("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/03_blaze_processed/raw/high_sensitivity/10x_D200-EP1-1_B2_matched_reads.fastq.gz")
 genome_fa <- file.path("/dcs04/hicks/data/sparthib/references/genome/GENCODE/GRCh38.p14.genome.fa")
 annot <- file.path("/dcs04/hicks/data/sparthib/references/genome/GENCODE/gencode.v44.chr_patch_hapl_scaff.annotation-short.gtf")
@@ -19,28 +18,9 @@ config_file <- FLAMES::create_config(config_dir, type = "sc_3end",
 #run minimap2
 
 if (!any(is.na(sys_which(c("minimap2", "k8"))))) {
-
-  config <- jsonlite::fromJSON(config_file)
-  genome_bam <- rownames(minimap2_align(
-    config = config, fa_file = genome_fa, fq_in = fastq, annot = annot,
-    outdir = outdir
-  ))
-  print("completed genome alignment")
-  
-  find_isoform(
-    annotation = annot, genome_fa = genome_fa,
-    genome_bam = genome_bam, outdir = outdir, config = config
-  )
-  print("completed isoform identification")
-  minimap2_realign(
-    config = config, fq_in = fastq,
-    outdir = outdir
-  )
-  print("created minimap2 realigned bam")
-  quantify_transcript(annotation = annot, outdir = outdir, config = config)
-  print("completed transcript quantification")
-  sce <- create_sce_from_dir(outdir = outdir, annotation = annot)
-  print("created SingleCellExperiment object")
+  sce <- sc_long_pipeline(
+    annotation = annot, fastq = fastq, genome_fa = genome_fa,
+    outdir = outdir, config_file = config_file, expect_cell_number = 63)
   
 }
 
@@ -49,6 +29,4 @@ sessionInfo()
 
 
 
-# sce <- sc_long_pipeline(
-#   annotation = annot, fastq = fastq, genome_fa = genome_fa,
-#   outdir = outdir, config_file = config_file, expect_cell_number = 63)
+
