7f1c6d79a8a66512538e995678e5ddb4838ea826
Sowmya Parthiban <sparthi1@jh.edu>
2024-07-02 12:17:49 -0400

    set `do_genome_alignment = FALSE`

--- code_single_cell/01_fastq_processing/flames_test.R
+++ code_single_cell/01_fastq_processing/flames_test.R
@@ -12,35 +12,28 @@ outdir <- file.path("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames
 config_dir <- file.path(outdir, "config")
 config_file <- FLAMES::create_config(config_dir, type = "sc_3end",
                                      do_barcode_demultiplex = FALSE,
+                                     do_genome_alignment = FALSE,
+                                     do_gene_quantification = FALSE,
                                      min_sup_cnt = 5,
                                      threads = 20)
-genome_bam <- file.path("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/align2genome.bam")
+# genome_bam <- file.path("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/align2genome.bam")
 
 #run minimap2
 
 if (!any(is.na(sys_which(c("minimap2", "k8"))))) {
-  # sce <- sc_long_pipeline(
-  #   annotation = annot, fastq = fastq, genome_fa = genome_fa,
-  #   outdir = outdir, config_file = config_file, expect_cell_number = 63)
-  
+  sce <- sc_long_pipeline(
+    annotation = annot, fastq = fastq,
+    genome_fa = genome_fa,
+    outdir = outdir, 
+    config_file = config_file,
+    expect_cell_number = 63)
+
   
   #sce pipeline failed because it didn't find the matched_reads.fastq file
   #https://github.com/mritchielab/FLAMES/issues/30
   #work around is to create a symlink to the blaze processed fastq file 
   #ln -s /dcs04/hicks/data/sparthib/retina_single_cell_lrs/03_blaze_processed/raw/high_sensitivity/10x_D200-EP1-1_B2_matched_reads.fastq /dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/matched_reads.fastq
-  config <- jsonlite::fromJSON(config_file)
-  
-  find_isoform(
-    annotation = annot, genome_fa = genome_fa,
-    genome_bam = genome_bam, outdir = outdir, config = config
-  )
-  minimap2_realign(
-    config = config, fq_in = fastq,
-    outdir = outdir
-  )
-  quantify_transcript(annotation = annot, outdir = outdir, config = config)
-  sce <- create_sce_from_dir(outdir = outdir, annotation = annot)
-  
+
 }
 
 
