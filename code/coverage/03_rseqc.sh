05cefd4bc586c218b871c5d866c0fd6b6a58837a
Sowmya Parthiban <sparthi1@jh.edu>
2024-07-08 18:49:29 -0400

    run RSeQC for all samples

--- code/coverage/03_rseqc.sh
+++ code/coverage/03_rseqc.sh
@@ -6,8 +6,10 @@
 #SBATCH --job-name=gene_body_cov
 #SBATCH --mail-user=sparthi1@jhu.edu
 #SBATCH --mail-type=ALL
-#SBATCH -o logs/plot_gene_body_cov_transcript_length.txt
-#SBATCH -e logs/plot_gene_body_cov_transcript_length.txt
+#SBATCH -o logs/plot_gene_body_cov_transcript_length.%a.txt
+#SBATCH -e logs/plot_gene_body_cov_transcript_length.%a.txt
+#SBATCH --array=1-15
+#SBATCH --time=3-00:00:00
 
 
 echo "**** Job starts ****"
@@ -17,13 +19,26 @@ echo "User: ${USER}"
 echo "Job id: ${SLURM_JOB_ID}"
 echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${SLURMD_NODENAME}"
-# echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
 echo "****"
 
+CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
+BAM_FOLDER=/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/primary_over_30_chr_only/
+sample=$(awk -v Index=${SLURM_ARRAY_TASK_ID} '$1==Index {print $2}' $CONFIG)
+echo "$sample"
+
 ref_gene_model=/dcs04/hicks/data/sparthib/references/rseqc_gencode_44_comp.bed
-bam_paths=/users/sparthib/retina_lrs/code/coverage/H9-FT2_bam_paths.txt
-path_to_output=/users/sparthib/retina_lrs/plots/coverage/H9_FT2_deciles
-mkdir -p $path_to_output
+bam_paths=/dcs04/hicks/data/sparthib/retina_lrs/05c_coverage/genome/isoquant/${sample}_deciles/${sample}_bam_paths.txt
+path_to_output=/users/sparthib/retina_lrs/plots/coverage/${sample}_deciles/
+
+cd /dcs04/hicks/data/sparthib/retina_lrs/05c_coverage/genome/isoquant/${sample}_deciles/
+rm $bam_paths
+files_array=($(ls *.bam))
+for file in "${files_array[@]}"; 
+do 
+    echo "/dcs04/hicks/data/sparthib/retina_lrs/05c_coverage/genome/isoquant/${sample}_deciles/$file" >> $bam_paths
+done
+
 
 ml load rseqc/3.0.1
 geneBody_coverage.py -i $bam_paths -r $ref_gene_model -o $path_to_output
