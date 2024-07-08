08f04fc0aa544084c043099a79e4d74e15747931
Sowmya Parthiban <sparthi1@jh.edu>
2024-07-08 13:28:56 -0400

    change time limit, increase mem, for all samples

--- code/coverage/02_split_bam_by_decile.sh
+++ code/coverage/02_split_bam_by_decile.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 #SBATCH -p shared
-#SBATCH --mem=30G
+#SBATCH --mem=50G
 #SBATCH -c 5
 #SBATCH --job-name=highqualbam
 #SBATCH --mail-user=sparthi1@jhu.edu
 #SBATCH --mail-type=ALL
 #SBATCH -o logs/split_bam_by_decile.txt
 #SBATCH -e logs/split_bam_by_decile.txt
-#SBATCH -t 7-00:00:00
+#SBATCH --array=1-15
+#SBATCH --time=3-00:00:00
 
 echo "**** Job starts ****"
 date +"%Y-%m-%d %T"
@@ -17,26 +18,36 @@ echo "User: ${USER}"
 echo "Job id: ${SLURM_JOB_ID}"
 echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${SLURMD_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+echo "****"
+
+CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
+BAM_FOLDER=/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/primary_over_30_chr_only/
+sample=$(awk -v Index=${SLURM_ARRAY_TASK_ID} '$1==Index {print $2}' $CONFIG)
+echo "$sample"
+
 
 ml load samtools 
 
+
 # Define the folder containing the decile files
-decile_folder=/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/isoquant/FT_RGC/OUT/H9_FT2_deciles
+decile_folder=/dcs04/hicks/data/sparthib/retina_lrs/05c_coverage/genome/isoquant/${sample}_deciles
+mkdir -p $decile_folder
 
 # Define the input BAM file
-input_bam=/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/primary_over_30_chr_only/H9-FT_2_primary_over_30_chr_only_sorted.bam
+input_bam=/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/primary_over_30_chr_only/${sample}_primary_over_30_chr_only_sorted.bam
 
 # Loop over each decile file and process it
 for i in {1..10}
 do
     # Define the current decile file and output BAM file
     decile_file=$decile_folder/decile_${i}.tsv
-    output_bam=$decile_folder/H9_FT2_genome_decile_${i}.bam
+    output_bam=$decile_folder/$decile_${i}_$sample.bam
 
     # Subset the BAM file based on the current decile file
-    # samtools view -h $input_bam | \
-    # awk 'NR==FNR {reads[$1]; next} ($1 in reads) || ($1 ~ /^@/)' $decile_file - | \
-    # samtools view -Sb - > $output_bam
+    samtools view -h $input_bam | \
+    awk 'NR==FNR {reads[$1]; next} ($1 in reads) || ($1 ~ /^@/)' $decile_file - | \
+    samtools view -Sb - > $output_bam
     samtools index $output_bam > ${output_bam}.bai
 done
 
