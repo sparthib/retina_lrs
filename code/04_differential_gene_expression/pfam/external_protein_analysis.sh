ca0a400b2b00c5ae0a97b05e7c50b4b68da681ad
Sowmya Parthiban <sparthi1@jh.edu>
2024-07-22 17:52:35 -0400

    update protein analyses

--- code/04_differential_gene_expression/pfam/external_protein_analysis.sh
+++ code/04_differential_gene_expression/pfam/external_protein_analysis.sh
@@ -18,15 +18,16 @@ echo "Job id: ${SLURM_JOB_ID}"
 echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${SLURMD_NODENAME}"
 
-comparisons=("FT_vs_RGC" "RO_D100_vs_RO_D45" "RO_D100_vs_RO_D200" "RO_D200_vs_RO_D45")
+# comparisons=("FT_vs_RGC" "RO_D100_vs_RO_D45" "RO_D100_vs_RO_D200" "RO_D200_vs_RO_D45")
+comparisons=("ROs")
 
 source activate CPC2
 cd $CPC_HOME
 for item in ${comparisons[@]}; do
     echo $item
-    NT_FASTA=/users/sparthib/retina_lrs/processed_data/dtu/DTU_gandall/bambu/$item/isoformSwitchAnalyzeR_isoform_nt.fasta
-    CPC2_OUTPUT=/users/sparthib/retina_lrs/processed_data/dtu/DTU_gandall/bambu/$item/CPC2_output
-    bin/CPC2.py -i $NT_FASTA -o $CPC2_OUTPUT 
+    NT_FASTA=/users/sparthib/retina_lrs/processed_data/dtu/DTU_gandall/bambu/$item/fastas/isoformSwitchAnalyzeR_isoform_nt.fasta
+    CPC2_OUTPUT=/users/sparthib/retina_lrs/processed_data/dtu/DTU_gandall/bambu/$item/external_protein_analyses
+    bin/CPC2.py -i $NT_FASTA -o $CPC2_OUTPUT
 done
 
 conda deactivate
@@ -35,13 +36,13 @@ conda deactivate
 #### pfam
 # https://vcru.wisc.edu/simonlab/bioinformatics/programs/install/pfamscan.htm
 
-source activate pfam 
+source activate pfam
 cd /dcs04/hicks/data/sparthib/retina_lrs/PfamScan/pfam_scan
 for item in ${comparisons[@]}; do
     echo $item
-    input_dir=/users/sparthib/retina_lrs/processed_data/dtu/DTU_gandall/bambu/$item
+    input_dir=/users/sparthib/retina_lrs/processed_data/dtu/DTU_gandall/bambu/$item/fastas/
     ./pfam_scan.py $input_dir/isoformSwitchAnalyzeR_isoform_AA_complete.fasta \
-    ../ -out $input_dir/pfam_results.csv -cpu $SLURM_CPUS_PER_TASK
+    ../ -out /users/sparthib/retina_lrs/processed_data/dtu/DTU_gandall/bambu/$item/external_protein_analyses/pfam_results.csv -cpu $SLURM_CPUS_PER_TASK
 done
 conda deactivate
 
@@ -50,9 +51,9 @@ conda deactivate
 source activate SignalP
 for item in ${comparisons[@]}; do
     echo $item
-    input_dir=/users/sparthib/retina_lrs/processed_data/dtu/DTU_gandall/bambu/$item
+    input_dir=/users/sparthib/retina_lrs/processed_data/dtu/DTU_gandall/bambu/$item/fastas/
     signalp6 --fastafile $input_dir/isoformSwitchAnalyzeR_isoform_AA_complete.fasta \
-    --organism eukarya --output_dir $input_dir --format txt --mode fast
+    --organism eukarya --output_dir /users/sparthib/retina_lrs/processed_data/dtu/DTU_gandall/bambu/$item/external_protein_analyses --format txt --mode fast
 done
 
 echo "**** Job ends ****"
