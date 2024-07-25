ed9ed09044c298727cafa9d4ee2569788b107138
Sowmya Parthiban <sparthi1@jhu.edu>
2024-07-25 13:27:56 -0400

    add code and modified git ignore

---
+++ plots/gene_body_percentile/long/.geneBodyCoverage.r
@@ -0,0 +1,6 @@
+
+
+pdf("/users/sparthib/retina_lrs/plots/gene_body_percentile/long/.geneBodyCoverage.curves.pdf")
+x=1:100
+icolor = colorRampPalette(c("#7fc97f","#beaed4","#fdc086","#ffff99","#386cb0","#f0027f"))(0)
+dev.off()
