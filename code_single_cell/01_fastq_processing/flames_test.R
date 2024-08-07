4bdbb3f5334e86d12c835db5006f1ca0e009ff61
Sowmya Parthiban <sparthi1@jh.edu>
2024-08-07 08:27:26 +0530

    run flames for all samples

--- code_single_cell/01_fastq_processing/flames_test.R
+++ code_single_cell/01_fastq_processing/flames_test.R
@@ -1,26 +1,26 @@
 library("FLAMES")
 library("sessioninfo")
 
-## blaze FASTQ files 
-
 #have all input files uncompressed (Related to this issue: https://github.com/mritchielab/FLAMES/issues/31)
-fastq <- file.path("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/03_blaze_processed/raw/high_sensitivity/10x_D200-EP1-1_B2_matched_reads.fastq")
+sample <- commandArgs(trailingOnly = TRUE)
+fastq <- file.path("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/03_blaze_processed/raw/high_sensitivity/", paste0(sample, "_matched_reads.fastq"))
 genome_fa <- file.path("/dcs04/hicks/data/sparthib/references/genome/GENCODE/GRCh38.p14.genome.fa")
 annot <- file.path("/dcs04/hicks/data/sparthib/references/genome/GENCODE/gencode.v44.chr_patch_hapl_scaff.annotation.gtf")
 minimap_path <- file.path("/users/sparthib/minimap2")
 
-outdir <- file.path("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output")
-config_dir <- file.path(outdir, "config")
-config_file <- FLAMES::create_config(config_dir, type = "sc_3end",
-                                     do_barcode_demultiplex = FALSE,
-                                     do_genome_alignment = FALSE,
-                                     do_gene_quantification = FALSE,
-                                     do_isoform_identification = FALSE,
-                                     do_read_realignment = FALSE,
-                                     do_transcript_quantification = TRUE,
-                                     min_sup_cnt = 5,
-                                     threads = 20)
-# genome_bam <- file.path("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/align2genome.bam")
+
+outdir <- file.path("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/", sample)
+config_file <- file.path("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/config/config_file_3766646.json")
+# config_file <- FLAMES::create_config(config_dir, type = "sc_3end",
+#                                      do_barcode_demultiplex = FALSE,
+#                                      do_genome_alignment = FALSE,
+#                                      do_gene_quantification = TRUE,
+#                                      do_isoform_identification = TRUE,
+#                                      do_read_realignment = TRUE,
+#                                      do_transcript_quantification = TRUE,
+#                                      min_sup_cnt = 5,
+#                                      threads = 20)
+genome_bam <- file.path("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/", paste0(sample, "_sorted.bam"))
 
 #run minimap2
 
@@ -31,13 +31,6 @@ if (!any(is.na(sys_which(c("minimap2", "k8"))))) {
     outdir = outdir, 
     config_file = config_file,
     expect_cell_number = 2000)
-
-  
-  #sce pipeline failed because it didn't find the matched_reads.fastq file
-  #https://github.com/mritchielab/FLAMES/issues/30
-  #work around is to create a symlink to the blaze processed fastq file 
-  #ln -s /dcs04/hicks/data/sparthib/retina_single_cell_lrs/03_blaze_processed/raw/high_sensitivity/10x_D200-EP1-1_B2_matched_reads.fastq /dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/matched_reads.fastq
-
   
   sce <- create_sce_from_dir(outdir = outdir, annotation = annot)
   
--- code_single_cell/01_fastq_processing/flames_test.sh
+++ code_single_cell/01_fastq_processing/flames_test.sh
@@ -5,10 +5,10 @@
 #SBATCH -c 20
 #SBATCH --mail-user=sparthi1@jhu.edu
 #SBATCH --mail-type=ALL
-#SBATCH -o logs/flames/10x_D200-EP1-1_B2.txt
-#SBATCH -e logs/flames/10x_D200-EP1-1_B2.txt
+#SBATCH -o logs/flames/.%a.txt
+#SBATCH -e logs/flames/.%a.txt
+#SBATCH --array=1-12
 #SBATCH --time=7-00:00:00
-
 
 echo "**** Job starts ****"
 date +"%Y-%m-%d %T"
@@ -17,9 +17,26 @@ echo "User: ${USER}"
 echo "Job id: ${SLURM_JOB_ID}"
 echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${SLURMD_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+CONFIG=/users/sparthib/retina_lrs/raw_data/single_cell.config
+sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
+num_cells=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $5}' $CONFIG)
+path=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $3}' $CONFIG)
+seq_sum=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $4}' $CONFIG)
+echo $sample
+
+#sce pipeline failed because it didn't find the matched_reads.fastq file
+  #https://github.com/mritchielab/FLAMES/issues/30
+  #work around is to create a symlink to the blaze processed fastq file 
+#create symlink for the fastq files 
+mkdir -p /dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/$sample
+gunzip -c /dcs04/hicks/data/sparthib/retina_single_cell_lrs/03_blaze_processed/raw/high_sensitivity/${sample}_matched_reads.fastq.gz > /dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/$sample/matched_reads.fastq
+ln -s /dcs04/hicks/data/sparthib/retina_single_cell_lrs/03_blaze_processed/raw/high_sensitivity/${sample}_matched_reads.fastq /dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/$sample/matched_reads.fastq
+
 
 ml load conda_R/4.4.x
-Rscript flames_test.R
+Rscript flames_test.R "$sample"
 
 echo "**** Job ends ****"
 date +"%Y-%m-%d %T"
