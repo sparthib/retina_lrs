650a3cb14dab685f8178639944d2e3285f9fb037
Sowmya Parthiban <sparthi1@jh.edu>
2024-08-27 16:27:44 -0400

    use `create_sce_from_outdir` function in FLAMES

--- code_single_cell/01_fastq_processing/05_flames.R
+++ code_single_cell/01_fastq_processing/05_flames.R
@@ -3,40 +3,26 @@ library("sessioninfo")
 
 #have all input files uncompressed (Related to this issue: https://github.com/mritchielab/FLAMES/issues/31)
 sample <- commandArgs(trailingOnly = TRUE)
-fastq <- file.path("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/", sample, "matched_reads.fastq")
+fastq <- paste0("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/", sample, "/matched_reads.fastq")
 genome_fa <- file.path("/dcs04/hicks/data/sparthib/references/genome/GENCODE/GRCh38.p14.genome.fa")
 annot <- file.path("/dcs04/hicks/data/sparthib/references/genome/GENCODE/gencode.v44.chr_patch_hapl_scaff.annotation.gtf")
 minimap_path <- file.path("/users/sparthib/minimap2")
+# genome_bam <- paste0("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/primary_over_30_chr_only/", 
+#                      sample, "_primary_over_30_chr_only_sorted.bam")
 
 
-outdir <- file.path("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/", sample)
+outdir <- paste0("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/", sample)
 config_dir <- file.path("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/config")
 config_file <- file.path("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/config/bambu_isoform_processing.json")
-#use genome_alignment.json for gene quantification
-#use bambu_isoform_processing.json for isoform alignment and quantification
-# config_file <- FLAMES::create_config(config_dir, type = "sc_3end",
-#                                      do_barcode_demultiplex = FALSE,
-#                                      do_genome_alignment = FALSE,
-#                                      do_gene_quantification = FALSE,
-#                                      do_isoform_identification = TRUE,
-#                                       bambu_isoform_identification = TRUE,
-#                                      do_read_realignment = TRUE,
-#                                      do_transcript_quantification = TRUE,
-#                                      min_sup_cnt = 5,
-#                                      threads = 20)
-genome_bam <- paste0("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/primary_over_30_chr_only/", 
-                       sample, "_primary_over_30_chr_only_sorted.bam")
-
-#run minimap2
 
 if (!any(is.na(sys_which(c("minimap2", "k8"))))) {
-  sce <- sc_long_pipeline(
-    annotation = annot, fastq = fastq,
-    genome_fa = genome_fa,
-    genome_bam = genome_bam,
-    outdir = outdir, 
-    config_file = config_file,
-    expect_cell_number = 2000)
+
+  # sce <- sc_long_pipeline(
+  #   annotation = annot, fastq = fastq,
+  #   genome_fa = genome_fa,
+  #   outdir = outdir, 
+  #   config_file = config_file,
+  #   expect_cell_number = 2000)
   
   sce <- create_sce_from_dir(outdir = outdir, annotation = annot)
   
