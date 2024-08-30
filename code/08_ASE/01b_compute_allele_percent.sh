0205d4b0f3b6b65e7a1f14263ce5ab85c5391822
Sowmya Parthiban <sparthi1@jh.edu>
2024-08-30 16:46:48 -0400

    change log name

--- code/08_ASE/01b_compute_allele_percent.sh
+++ code/08_ASE/01b_compute_allele_percent.sh
@@ -3,11 +3,11 @@
 #SBATCH -p shared
 #SBATCH --mem=20G
 #SBATCH -c 5
-#SBATCH --job-name=longshot
+#SBATCH --job-name=compute_HP
 #SBATCH --mail-user=sparthi1@jhu.edu
 #SBATCH --mail-type=ALL
-#SBATCH -o logs/longshot.%a.txt
-#SBATCH -e logs/longshot.%a.txt
+#SBATCH -o logs/compute_HP.%a.txt
+#SBATCH -e logs/compute_HP.%a.txt
 #SBATCH --array=1-15
 #SBATCH --time=7-00:00:00
 
