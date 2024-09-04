1053b1f4ca3a313aa6593e7c9badb502da4de33f
Sowmya Parthiban <sparthi1@jh.edu>
2024-09-04 15:48:58 -0400

    convert R to bash code

--- code/03_bam_QC/figures/01_exon_exon_junctions.sh
+++ code/03_bam_QC/figures/01_exon_exon_junctions.sh
@@ -27,8 +27,30 @@ CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
 sample=$(awk -v Index=${SLURM_ARRAY_TASK_ID} '$1==Index {print $2}' $CONFIG)
 echo "$sample"
 
-ml load conda_R/4.4.x
-Rscript 01_exon_exon_junctions.R $sample
+ml load samtools
+
+alignment_dir='/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/primary_over_30_chr_only'
+alignment="${alignment_dir}/${sample}_primary_over_30_chr_only_sorted.bam"
+
+# 1. Define the function to compute the number of exon-exon junctions covered by one read based on CIGAR string
+function compute_num_junction_per_read() {
+    local cigar_string=$1
+    local num_junctions=$(echo "$cigar_string" | grep -o "N" | wc -l)
+    echo $num_junctions
+}
+
+# 2. Compute number of junctions in read
+output_file="/users/sparthib/retina_lrs/processed_data/exon_exon/${sample}_num_junctions.txt"
+nums=()
+
+samtools view "$alignment" | awk '{print $6}' | while read cigar; do
+    num=$(compute_num_junction_per_read "$cigar")
+    nums+=("$num")
+done
+
+# 3. Generate the table of counts
+printf "%s\n" "${nums[@]}" | sort | uniq -c | awk '{print $2, $1}' > "$output_file"
+
 
 echo "**** Job ends ****"
 date +"%Y-%m-%d %T"
