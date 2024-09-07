c14b0f3654497e8ff1c27cf4b0d0015e952eeca9
Sowmya Parthiban <sparthi1@jh.edu>
2024-09-07 18:33:59 -0400

    count distribution

---
+++ code/03_bam_QC/figures/03_count_distribution.py
@@ -0,0 +1,53 @@
+import argparse
+from collections import Counter
+
+# Function to process the input file and extract counts
+def process_count_file(input_file):
+  counts = []
+
+with open(input_file, 'r') as file:
+  for line in file:
+  line = line.strip()  # Remove extra whitespace/newlines
+# Split the line by comma and extract the count (second element)
+try:
+  _, count = line.split(',')
+count = int(count)  # Convert the count to an integer
+counts.append(count)
+except ValueError:
+  print(f"Invalid line or format: {line}")
+
+# Count the frequency of each unique count
+count_frequencies = Counter(counts)
+
+return count_frequencies
+
+# Function to write the unique counts and their frequencies to an output file
+def write_count_frequencies_to_file(output_file, count_frequencies):
+  with open(output_file, 'w') as out_f:
+  out_f.write("Unique_Count,Frequency\n")
+for count, freq in sorted(count_frequencies.items()):
+  out_f.write(f"{count},{freq}\n")
+
+print(f"Count frequencies written to {output_file}")
+
+# Main function to handle argument parsing
+def main():
+  parser = argparse.ArgumentParser(description="Compute frequency distribution of counts from input file")
+
+# Define the input argument for the sample file
+parser.add_argument("input_file", help="Path to the input file containing CIGAR strings and counts")
+
+# Optional argument for the output file (default name if not provided)
+parser.add_argument("-o", "--output_file", default="count_distribution_output.txt",
+                    help="Path to the output text file (default: count_distribution_output.txt)")
+
+args = parser.parse_args()
+
+# Process the file to get count frequencies
+count_frequencies = process_count_file(args.input_file)
+
+# Write the results to the output file
+write_count_frequencies_to_file(args.output_file, count_frequencies)
+
+if __name__ == "__main__":
+  main()
---
+++ code/03_bam_QC/figures/03_count_distribution.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH --mem=20G
+#SBATCH -c 5
+#SBATCH --job-name=count_distribution
+#SBATCH --mail-user=sparthi1@jhu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o logs/count_distribution/%a.txt
+#SBATCH -e logs/count_distribution/%a.txt
+#SBATCH --array=1-15
+#SBATCH --time=7-00:00:00
+
+
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
+sample=$(awk -v Index=${SLURM_ARRAY_TASK_ID} '$1==Index {print $2}' $CONFIG)
+echo "$sample"
+
+input_file="/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/primary_over_30_chr_only/CIGAR_sequences/${sample}_N_count.txt"
+output_file="/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/primary_over_30_chr_only/CIGAR_sequences/${sample}_N_count_distribution.txt"
+
+ml load python/3.11.8
+python3 03_count_distribution.py ${input_file} -o ${output_file}
+
+echo "**** Job ends ****"
+date +"%Y-%m-%d %T"
