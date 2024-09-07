8fb1498ff5492084ead275b015af907040bf882f
Sowmya Parthiban <sparthi1@jh.edu>
2024-09-06 22:48:04 -0400

    add python script to count N

--- code/03_bam_QC/figures/02_count_N.sh
+++ code/03_bam_QC/figures/02_count_N.sh
@@ -3,14 +3,13 @@
 #SBATCH -p shared
 #SBATCH --mem=20G
 #SBATCH -c 5
-#SBATCH --job-name=exon_exon_junctions
+#SBATCH --job-name=count_N
 #SBATCH --mail-user=sparthi1@jhu.edu
 #SBATCH --mail-type=ALL
-#SBATCH -o logs/exon_exon/sample.%a.txt
-#SBATCH -e logs/exon_exon/sample.%a.txt
+#SBATCH -o logs/count_N/sample.%a.txt
+#SBATCH -e logs/count_N/sample.%a.txt
 #SBATCH --array=1-15
 #SBATCH --time=7-00:00:00
-
 
 
 echo "**** Job starts ****"
@@ -26,14 +25,12 @@ CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
 sample=$(awk -v Index=${SLURM_ARRAY_TASK_ID} '$1==Index {print $2}' $CONFIG)
 echo "$sample"
 
-ml load samtools
+input_file="/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/primary_over_30_chr_only/CIGAR_sequences/${sample}_cigar_sequences.txt"
+output_file="/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/primary_over_30_chr_only/CIGAR_sequences/${sample}_N_count.txt"
 
-alignment_dir='/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/primary_over_30_chr_only'
-alignment="${alignment_dir}/${sample}_primary_over_30_chr_only_sorted.bam"
-
-output_file="/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/primary_over_30_chr_only/CIGAR_sequences/${sample}_cigar_sequences.txt"
-
-samtools view $alignment | awk '{print $6}' > $output_file
+ml load python/3.11.8
+python3 count_N.py ${input_file} -o ${output_file}
 
 echo "**** Job ends ****"
 date +"%Y-%m-%d %T"
+
