7420e3aeaa2acc5d1b2237a3f31e711dc63dd969
Sowmya Parthiban <sparthi1@jh.edu>
2024-09-09 17:52:32 -0400

    run transcript quant function for sample 8 and 9

---
+++ code_single_cell/01_fastq_processing/05b_flames_quantify_transcript.R
@@ -0,0 +1,22 @@
+library("FLAMES")
+library("sessioninfo")
+
+#have all input files uncompressed (Related to this issue: https://github.com/mritchielab/FLAMES/issues/31)
+sample <- commandArgs(trailingOnly = TRUE)
+fastq <- paste0("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/", sample, "/matched_reads.fastq")
+genome_fa <- file.path("/dcs04/hicks/data/sparthib/references/genome/GENCODE/GRCh38.p14.genome.fa")
+annot <- file.path("/dcs04/hicks/data/sparthib/references/genome/GENCODE/gencode.v44.chr_patch_hapl_scaff.annotation.gtf")
+minimap_path <- file.path("/users/sparthib/minimap2")
+# genome_bam <- paste0("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/primary_over_30_chr_only/", 
+#                      sample, "_primary_over_30_chr_only_sorted.bam")
+
+
+outdir <- paste0("/dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/", sample)
+config <- jsonlite::fromJSON(create_config(outdir, bambu_isoform_identification = TRUE))
+
+quantify_transcript(annotation = annot, outdir, config, pipeline = "sc_single_sample")
+
+print("done quantifying transcripts")
+
+sessionInfo()
+
