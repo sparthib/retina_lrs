8ded70699e32c92b1c7a0507e12330a30b12ed97
Sowmya Parthiban <sparthi1@jh.edu>
2024-09-09 22:52:13 -0400

    quantify gene

---
+++ code_single_cell/01_fastq_processing/05c_quantify_gene.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#SBATCH -p shared
+#SBATCH --mem=100G
+#SBATCH --job-name=flames
+#SBATCH -c 20
+#SBATCH --mail-user=sparthi1@jhu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o logs/flames/flames.%a.txt
+#SBATCH -e logs/flames/flames.%a.txt
+#SBATCH --array=7
+#SBATCH --time=7-00:00:00
+
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+CONFIG=/users/sparthib/retina_lrs/raw_data/single_cell.config
+sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
+num_cells=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $5}' $CONFIG)
+path=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $3}' $CONFIG)
+seq_sum=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $4}' $CONFIG)
+echo $sample
+
+#RUNNING JUST QUANTIFY TRANSCRIPT FUNCTION FOR 8 AND 9
+
+# fastq files have to unzipped and in the flames output directory folder named "matched_reads.fastq"
+#sce pipeline failed because it didn't find the matched_reads.fastq file
+  #https://github.com/mritchielab/FLAMES/issues/30
+  #work around is to create a symlink to the blaze processed fastq file 
+
+# rm -r /dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/$sample
+# mkdir -p /dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/$sample
+# gunzip -c /dcs04/hicks/data/sparthib/retina_single_cell_lrs/03_blaze_processed/raw/high_sensitivity/${sample}_matched_reads.fastq.gz > /dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/$sample/matched_reads.fastq
+
+
+ml load conda_R/4.4.x
+Rscript 05c_flames_quantify_gene.R "$sample"
+
+echo "**** Job ends ****"
+date +"%Y-%m-%d %T"
