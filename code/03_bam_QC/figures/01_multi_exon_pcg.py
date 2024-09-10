a50293805f4d6e165085b3631d73cc54a3f00806
Sowmya Parthiban <sparthi1@jh.edu>
2024-09-10 10:24:48 -0400

    multi exon pcg

---
+++ code/03_bam_QC/figures/01_multi_exon_pcg.py
@@ -0,0 +1,86 @@
+import pysam
+from collections import Counter
+import pandas as pd
+import numpy as np
+import os
+import pyranges as pr
+
+#from https://github.molgen.mpg.de/MayerGroup/NGN3_paper_code/blob/main/ONT_seq_analysis/long_short_comparison/junction_per_read.py
+# Load the GTF file
+gtf_fn = "/dcs04/hicks/data/sparthib/references/genome/GENCODE/gencode.v44.chr_patch_hapl_scaff.annotation.gtf"
+gtf = pr.read_gtf(gtf_fn, as_df = True)
+
+curr_gene = ''
+goi = list()
+curr_num_exon = 0
+for i in range(gtf.shape[0]):
+    if gtf.iloc[i,2] == 'gene' and gtf.iloc[i,9] == 'protein_coding':
+        if curr_num_exon > 1:
+            goi.append(curr_gene)
+        curr_gene = gtf.iloc[i,8]
+        curr_num_exon = 0
+    if gtf.iloc[i,2] == 'exon' and gtf.iloc[i,8] == curr_gene:
+        curr_num_exon += 1
+
+
+genic_gtf = gtf[gtf['Feature'] == 'gene']
+genic_gtf = genic_gtf[genic_gtf['gene_id'].isin(goi)]
+genic_gtf = genic_gtf.iloc[:, 0:14]
+
+###Get bam file names
+alignment_dir = '/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/primary_over_30_chr_only'
+alignment = list()
+
+for file in os.listdir(alignment_dir):
+    if file.endswith("chr_only_sorted.bam"):
+        alignment.append(os.path.join(alignment_dir, file))
+alignment.sort()
+
+
+###Define the function to compute the numebr of exon-exon junctions covered by one read based on CIGAR string
+def compute_num_junction_per_read(cigar_string):
+    
+    #m: number of exon-exon splice junctions
+    m = cigar_string.count('N')
+        
+    return m
+  
+
+
+###Calculate for each bam file
+max_junction = 11 #can be changed
+nums = list()
+df_dic = {}
+# whole_cigar = ''
+for fn in alignment:
+    bamfile = pysam.AlignmentFile(fn, "rb") 
+    for i in range(genic_gtf.shape[0]):
+    # for read in bamfile.fetch('chr3', 149964904, 149970895): #PFN2, at most 2 exon-exon junctions
+    # for read in bamfile.fetch('chr14', 69767112, 69772005): #SRSF5, at most 8 exon-exon junctions
+        for read in bamfile.fetch(genic_gtf.iloc[i,0], genic_gtf.iloc[i,3], genic_gtf.iloc[i,4]):  #whole genome
+        #     print(read.mapq)
+        #     print(read)
+        #     break
+    #         name = read.query_name
+    #         names.append(name)
+    #         mq = read.mapq
+    #         mqs.append(mq)
+            num = compute_num_junction_per_read(read.cigarstring)
+            nums.append(num)
+    #         cigar = read.cigarstring
+    #         whole_cigar = whole_cigar + cigar
+    #     print(Counter(nums))
+    tmp_counter = Counter(nums)
+    # print(tmp_counter)
+    tmp_num = list()
+    for i in range(max_junction):   
+        tmp_num.append(tmp_counter[i])
+    df_dic[fn] = tmp_num    
+    nums = list()
+
+df = pd.DataFrame(df_dic).transpose()
+per_df = df.div(df.sum(axis=1), axis=0)
+per_df.to_csv('/users/sparthib/retina_lrs/processed_data/exon_exon/junction_per_read.csv')
+
+
+  
---
+++ code/03_bam_QC/figures/01_multi_exon_pcg.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH --mem=150G
+#SBATCH -c 20
+#SBATCH --job-name=multi_exon_pcg
+#SBATCH --mail-user=sparthi1@jhu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o logs/multi_exon_pcg/%a.txt
+#SBATCH -e logs/multi_exon_pcg/%a.txt
+#SBATCH --time=7-00:00:00
+
+
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+
+ml load python/3.11.8
+python3 01_multi_exon_pcg.py 
+
+echo "**** Job ends ****"
+date +"%Y-%m-%d %T"
