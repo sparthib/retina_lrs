4fa8fc50fa8d688a0de542acda2243c3ecfe365b
Sowmya Parthiban <sparthi1@jh.edu>
2024-09-22 18:36:05 -0400

    move scripts, create script to add extra barcode column

---
+++ code_single_cell/01_fastq_processing/isoquant/01_add_barcode_tags.py
@@ -0,0 +1,36 @@
+import pysam
+import argparse
+
+# Set up command-line argument parsing
+parser = argparse.ArgumentParser(description="Add CB and UMI tags to BAM reads.")
+parser.add_argument("input_bam", help="Input BAM file")
+parser.add_argument("output_bam", help="Output BAM file with tags added")
+args = parser.parse_args()
+
+# Open the BAM file
+input_bam = "your_input.bam"  # Replace with your input BAM file path
+output_bam = "output_with_tags.bam"  # Replace with your desired output BAM file path
+
+# Open the input BAM file in read mode
+bamfile = pysam.AlignmentFile(input_bam, "rb")
+# Open a new BAM file for writing, using the same header as the input
+output_bamfile = pysam.AlignmentFile(output_bam, "wb", header=bamfile.header)
+
+for read in bamfile:
+    # Get the read name (e.g., CATTGAGGTGATGAAT_AAATCTGCTTGT#109dbc01-6a19-4ee7-9d45-1a42a1584da7_-)
+    read_name = read.query_name
+    
+    # Split the read name into barcode and UMI
+    barcode, rest = read_name.split('_', 1)
+    umi = rest.split('#')[0]  # UMI is before the '#'
+
+    # Add the CB (cell barcode) and UMI tags
+    read.set_tag("CB", barcode, value_type='Z')  # 'Z' indicates a string tag
+    read.set_tag("UM", umi, value_type='Z')
+
+    # Write the modified read to the new BAM file
+    output_bamfile.write(read)
+
+# Close the files
+bamfile.close()
+output_bamfile.close()
--- code_single_cell/01_fastq_processing/isoquant/01_add_barcode_tags.sh
+++ code_single_cell/01_fastq_processing/isoquant/01_add_barcode_tags.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH -p shared
-#SBATCH --mem=100G
+#SBATCH --mem=150G
 #SBATCH --job-name=flames
 #SBATCH -c 20
 #SBATCH --mail-user=sparthi1@jhu.edu
 #SBATCH --mail-type=ALL
-#SBATCH -o logs/flames/flames.%a.txt
-#SBATCH -e logs/flames/flames.%a.txt
-#SBATCH --array=7
+#SBATCH -o logs/flames/flames_min_count2.%a.txt
+#SBATCH -e logs/flames/flames_min_count2.%a.txt
+#SBATCH --array=1-12
 #SBATCH --time=7-00:00:00
 
 echo "**** Job starts ****"
@@ -17,7 +17,7 @@ echo "User: ${USER}"
 echo "Job id: ${SLURM_JOB_ID}"
 echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${SLURMD_NODENAME}"
-echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+echo "Array task ID: ${SLURM_ARRAY_TASK_ID}"
 
 CONFIG=/users/sparthib/retina_lrs/raw_data/single_cell.config
 sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
@@ -26,14 +26,13 @@ path=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $3}' $CONFIG)
 seq_sum=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $4}' $CONFIG)
 echo $sample
 
-#RUNNING JUST QUANTIFY GENE FUNCTION FOR SAMPLE 7
+input_bam=/dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/primary_over_30_chr_only/${sample}_primary_over_30_chr_only_sorted.bam
+output_bam=/dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/primary_over_30_chr_only/${sample}_with_tags.bam
 
-#create symlink for bam file 
-# ln -s /dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/${sample}_sorted.bam /dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/10X_D200-EP1-1_B1/align2genome.bam
-# ln -s /dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/${sample}_sorted.bam.bai /dcs04/hicks/data/sparthib/retina_single_cell_lrs/05_flames_output/10X_D200-EP1-1_B1/align2genome.bam.bai
+ml load python/3.10.13
+python3 add_tags_to_bam.py your_input.bam output_with_tags.bam
 
-ml load conda_R/4.4.x
-Rscript 05c_flames_quantify_gene.R "${sample}" 
+
 
 echo "**** Job ends ****"
 date +"%Y-%m-%d %T"
