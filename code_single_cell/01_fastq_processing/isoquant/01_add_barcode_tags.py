a6874412b15ed2174ed063f9711f7b02f75d717d
Sowmya Parthiban <sparthi1@jh.edu>
2024-09-26 19:43:39 -0400

    dedup UMIs

--- code_single_cell/01_fastq_processing/isoquant/01_add_barcode_tags.py
+++ code_single_cell/01_fastq_processing/isoquant/01_add_barcode_tags.py
@@ -2,19 +2,22 @@ import pysam
 import argparse
 
 # Set up command-line argument parsing
-parser = argparse.ArgumentParser(description="Add CB and UMI tags to BAM reads.")
+parser = argparse.ArgumentParser(description="Add CB and UMI tags to BAM reads and remove duplicates.")
 parser.add_argument("input_bam", help="Input BAM file")
 parser.add_argument("output_bam", help="Output BAM file with tags added")
 args = parser.parse_args()
 
-# Open the BAM file
+# Open the input and output BAM files
 input_bam = args.input_bam
-output_bam = args.output_bam  
+output_bam = args.output_bam
 
 # Open the input BAM file in read mode
 bamfile = pysam.AlignmentFile(input_bam, "rb")
 # Open a new BAM file for writing, using the same header as the input
 output_bamfile = pysam.AlignmentFile(output_bam, "wb", header=bamfile.header)
+
+# Set to store unique (barcode, UMI) pairs
+unique_barcodes_umis = set()
 
 for read in bamfile:
     # Get the read name (e.g., CATTGAGGTGATGAAT_AAATCTGCTTGT#109dbc01-6a19-4ee7-9d45-1a42a1584da7_-)
@@ -24,12 +27,18 @@ for read in bamfile:
     barcode, rest = read_name.split('_', 1)
     umi = rest.split('#')[0]  # UMI is before the '#'
 
-    # Add the CB (cell barcode) and UMI tags
-    read.set_tag("CB", barcode, value_type='Z')  # 'Z' indicates a string tag
-    read.set_tag("UM", umi, value_type='Z')
-
-    # Write the modified read to the new BAM file
-    output_bamfile.write(read)
+    # Check if the (barcode, UMI) pair is unique
+    if (barcode, umi) not in unique_barcodes_umis:
+        # Add the CB (cell barcode) and UMI tags
+        read.set_tag("CB", barcode, value_type='Z')  # 'Z' indicates a string tag
+        read.set_tag("UM", umi, value_type='Z')
+        
+        # Add this pair to the set of seen (barcode, UMI) pairs
+        unique_barcodes_umis.add((barcode, umi))
+        
+        # Write the modified read to the new BAM file
+        output_bamfile.write(read)
+    # If the pair is not unique, skip this read
 
 # Close the files
 bamfile.close()
--- code_single_cell/01_fastq_processing/isoquant/01_add_barcode_tags.sh
+++ code_single_cell/01_fastq_processing/isoquant/01_add_barcode_tags.sh
@@ -27,15 +27,15 @@ seq_sum=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $4}' $CONFIG)
 echo $sample
 
 input_bam=/dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/primary_over_30_chr_only/${sample}_primary_over_30_chr_only_sorted.bam
-output_bam=/dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/primary_over_30_chr_only/${sample}_with_tags.bam
+output_bam=/dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/primary_over_30_chr_only/tagged/${sample}_deduped.bam
 ml load python/3.10.13
 python3 01_add_barcode_tags.py $input_bam $output_bam
 
 
 #sort tagged file 
-input_bam=/dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/primary_over_30_chr_only/${sample}_with_tags.bam
-output_bam=/dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/primary_over_30_chr_only/${sample}_with_tags_sorted.bam
-output_bai=/dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/primary_over_30_chr_only/${sample}_with_tags_sorted.bam.bai
+input_bam=/dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/primary_over_30_chr_only/tagged/${sample}_deduped.bam
+output_bam=/dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/primary_over_30_chr_only/tagged/${sample}_deduped_sorted.bam
+output_bai=/dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/primary_over_30_chr_only/tagged/${sample}_deduped_sorted.bam.bai
 ml load samtools
 
 samtools sort $input_bam -o $output_bam
