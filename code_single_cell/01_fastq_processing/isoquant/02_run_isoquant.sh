e6eaab8b9d55b5afeaa0e3e43cc38cbbd91d71a2
Sowmya Parthiban <sparthi1@jh.edu>
2024-09-27 09:52:00 -0400

    run isoquant on deduped BAMs

--- code_single_cell/01_fastq_processing/isoquant/02_run_isoquant.sh
+++ code_single_cell/01_fastq_processing/isoquant/02_run_isoquant.sh
@@ -9,7 +9,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH -o logs/isoquant.%a.txt
 #SBATCH -e logs/isoquant.%a.txt
-#SBATCH --array=5
+#SBATCH --array=1-12
 #SBATCH --time=7-00:00:00
 
 #try running for all chromosomes
@@ -31,15 +31,16 @@ sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
 echo $sample
 
 
-BAM_FOLDER=/dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/primary_over_30_chr_only
+BAM_FOLDER=/dcs04/hicks/data/sparthib/retina_single_cell_lrs/04_minimap2_output/genome/bams/primary_over_30_chr_only/tagged
 
 REFERENCE_GTF=/dcs04/hicks/data/sparthib/references/genome/GENCODE/gencode.v44.chr_patch_hapl_scaff.annotation.gtf.gz
 REFERENCE_FASTA=/dcs04/hicks/data/sparthib/references/genome/GENCODE/GRCh38.p14.genome.fa.gz
 
 OUTPUT_FOLDER=/dcs04/hicks/data/sparthib/retina_single_cell_lrs/quantification_alternatives/01_IsoQuant/$sample
+rm -r $OUTPUT_FOLDER
 mkdir -p $OUTPUT_FOLDER
 
-isoquant.py -d ont --bam ${BAM_FOLDER}/${sample}_with_tags_sorted.bam \
+isoquant.py -d ont --bam ${BAM_FOLDER}/${sample}_deduped_sorted.bam \
 --reference $REFERENCE_FASTA --genedb $REFERENCE_GTF --complete_genedb \
 --output $OUTPUT_FOLDER -t ${SLURM_CPUS_PER_TASK} \
 --sqanti_output --check_canonical --count_exons --bam_tags CB \
