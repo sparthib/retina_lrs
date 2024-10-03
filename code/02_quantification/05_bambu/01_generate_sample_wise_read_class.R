2f93dba58b33984609b730ef66268fbd51709d61
Sowmya Parthiban <sparthi1@jh.edu>
2024-10-03 16:23:07 -0400

    change annotation

--- code/02_quantification/05_bambu/01_generate_sample_wise_read_class.R
+++ code/02_quantification/05_bambu/01_generate_sample_wise_read_class.R
@@ -4,13 +4,13 @@ library(readr)
 library(sessioninfo)
 library(dplyr)
 
-# gtf.file <- "/dcs04/hicks/data/sparthib/references/genome/GENCODE/gencode.v44.chr_patch_hapl_scaff.annotation.gtf"
-# bambuAnnotations <- prepareAnnotations(gtf.file)
-# saveRDS(bambuAnnotations, "/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/annotations.rds")
+gtf.file <- "/dcs04/hicks/data/sparthib/references/genome/GENCODE/primary_assembly/release_46_primary_assembly.gtf"
+bambuAnnotations <- prepareAnnotations(gtf.file)
+saveRDS(bambuAnnotations, "/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/primary_assembly/annotations.rds")
 
 sample <- commandArgs(trailingOnly = TRUE)
 
-annotation <- readRDS("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/annotations.rds")
+annotation <- readRDS("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/primary_assembly/annotations.rds")
 bam_dir <- "/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_assembly/high_quality"
 bam_dir <- paste0(bam_dir, sample, "_primary_over_30_sorted.bam")
 fa.file <- "/dcs04/hicks/data/sparthib/references/genome/GENCODE/primary_assembly/release_46_primary_genome.fa"
@@ -26,17 +26,6 @@ se_read_class <- bambu(reads = paste0(bam_dir),
                      annotations = annotation,
                      genome = fa.file,
                      rcOutDir = output_dir,
-                     lowMemory = TRUE,
-                     trackReads = TRUE)
-
+                     lowMemory = TRUE)
 
 sessioninfo::session_info()
-
-
-# class(metadata(se)$readToTranscriptMaps)
-# colnames(metadata(se)$readToTranscriptMaps[[1]])
-# 
-# metadata(se)$readToTranscriptMaps[1][1]
-# length(metadata(se)$readToTranscriptMaps[[7]])
-
-# temp <- readRDS("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/track_reads/EP1-WT_hRO_2/25dd8655e094e7_25dd8655e094e7.rds")
