da0d4fa3a2c51f30c37e6b4858455d5b4b49fcec
Sowmya Parthiban <sparthi1@jh.edu>
2024-10-03 11:56:45 -0400

    change input dir

--- code/08_ASE/short_reads/bowtie.sh
+++ code/08_ASE/short_reads/bowtie.sh
@@ -19,10 +19,11 @@ echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${SLURMD_NODENAME}"
 echo "****"
 
-REFERENCE_FASTA=/dcs04/hicks/data/sparthib/references/genome/GENCODE/primary_assembly/release_46_primary_genome.fa.gz
+INPUT_DIR=/dcs04/hicks/data/sparthib/retina_lrs/09_ASE/H9_DNA_Seq_data/fastq
 OUTPUT_DIR=/dcs04/hicks/data/sparthib/retina_lrs/09_ASE/H9_DNA_Seq_data/sams
-ml load bowtie/2.5.1
 
+
+ml load bowtie/2.5.1
 BT_INDEX_PATH=/dcs04/hicks/data/sparthib/retina_lrs/09_ASE/H9_DNA_Seq_data/GRCh38_noalt_as/GRCh38_noalt_as
 
 # Aligning paired reads
@@ -31,7 +32,7 @@ samples=(SRR1091088 SRR1091091 SRR1091092)
 for sample in ${samples[@]}
 do
     bowtie2 -x $BT_INDEX_PATH -p ${SLURM_CPUS_PER_TASK} \
-    -1 example/reads/${sample}_1.fq -2 example/reads/${sample}_2.fq -S $OUTPUT_DIR/${sample}.sam
+    -1 $INPUT_DIR/${sample}_1.fq -2 $INPUT_DIR/${sample}_2.fq -S $OUTPUT_DIR/${sample}.sam
 done
 
 echo "**** Job ends ****"
