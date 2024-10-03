325d61f10d25f0b5ea6c80f9ee9e060874055330
Sowmya Parthiban <sparthi1@jh.edu>
2024-10-03 17:20:51 -0400

    try on only chromosomes

--- code/02_quantification/05_bambu/01_generate_sample_wise_read_class.R
+++ code/02_quantification/05_bambu/01_generate_sample_wise_read_class.R
@@ -12,11 +12,17 @@ sample <- commandArgs(trailingOnly = TRUE)
 
 annotation <- readRDS("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/primary_assembly/annotations.rds")
 bam_dir <- "/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_assembly/high_quality"
-bam_dir <- paste0(bam_dir, sample, "_primary_over_30_sorted.bam")
+bam_dir <- paste0(bam_dir, sample, "_primary_over_30_chr_only_sorted.bam")
 fa.file <- "/dcs04/hicks/data/sparthib/references/genome/GENCODE/primary_assembly/release_46_primary_genome.fa"
 
 output_dir <- paste0("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/primary_assembly/", sample, "/")
 
+#check if bam file exists
+if (!file.exists(bam_dir)){
+  stop("Bam file does not exist")
+}
+
+#create output directory
 if (!dir.exists(output_dir)){
   dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
 }
