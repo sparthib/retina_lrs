649b8d40e7e135afa5b96313b598a86c7001e16d
Sowmya Parthiban <sparthi1@jh.edu>
2024-10-04 10:14:45 -0400

    filter short read bams

---
+++ code/08_ASE/filter_bams.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH --mem=100G
+#SBATCH -c 20
+#SBATCH --job-name=bowtie
+#SBATCH --mail-user=sparthi1@jhu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o logs/bowtie.txt
+#SBATCH -e logs/bowtie.txt
+#SBATCH -t 7-00:00:00
+
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "****"
+
+
+INPUT_DIR=/dcs04/hicks/data/sparthib/retina_lrs/09_ASE/H9_DNA_Seq_data/sams
+OUTPUT_DIR=/dcs04/hicks/data/sparthib/retina_lrs/09_ASE/H9_DNA_Seq_data/filtered_bams
+
+ml load samtools
+
+samples=(SRR1091088 SRR1091091 SRR1091092)
+
+for sample in ${samples[@]}
+do
+    samtools view -bS -h -F 256 -q 20 $INPUT_DIR/${sample}.sam > $OUTPUT_DIR/${sample}.bam
+    samtools sort $OUTPUT_DIR/${sample}.bam -o $OUTPUT_DIR/${sample}.sorted.bam
+    samtools index $OUTPUT_DIR/${sample}.sorted.bam $OUTPUT_DIR/${sample}.sorted.bam.bai
+    rm $OUTPUT_DIR/${sample}.bam
+done
+
+echo "**** Job ends ****"
+date +"%Y-%m-%d %T"
+
+
+
+
+
+
+
+
+
+
+samtools view -h -F 256 -q 20 file.bam
