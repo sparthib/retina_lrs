4ce0052c1882e9b3797291ac39f4ff1616f6a01a
Sowmya Parthiban <sparthi1@jh.edu>
2024-10-04 08:29:40 -0400

    remove bam related code and delete old folder

--- code/02_quantification/07_flames/01_flames.R
+++ code/02_quantification/07_flames/01_flames.R
@@ -5,7 +5,6 @@ sample <- commandArgs(trailingOnly = TRUE)
 gtf <- "/dcs04/hicks/data/sparthib/references/genome/GENCODE/primary_assembly/release_46_primary_assembly.gtf"
 genome_fa  <- "/dcs04/hicks/data/sparthib/references/genome/GENCODE/primary_assembly/release_46_primary_genome.fa"
 outdir <- paste0("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/", sample)
-genome_bam <- "/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/GENCODE_splice/H9-hRGC_1_sorted.bam"
 minimap_path <- file.path("/users/sparthib/minimap2")
 fastq <- paste0(output_dir, "/matched_reads.fastq")
 
