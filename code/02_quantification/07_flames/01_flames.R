770b671e8ab535d8c2ac7f5d5e590c0176e53d95
Sowmya Parthiban <sparthi1@jh.edu>
2024-10-09 19:17:29 -0400

    reorder bash scripts

--- code/02_quantification/07_flames/01_flames.R
+++ code/02_quantification/07_flames/01_flames.R
@@ -6,7 +6,7 @@ gtf <- "/dcs04/hicks/data/sparthib/references/genome/GENCODE/primary_assembly/re
 genome_fa  <- "/dcs04/hicks/data/sparthib/references/genome/GENCODE/primary_assembly/release_46_primary_genome.fa"
 outdir <- paste0("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/", sample)
 minimap_path <- file.path("/users/sparthib/minimap2")
-
+fastq <- paste0(outdir, "/matched_reads.fastq")
 #config_file <- FLAMES::create_config(outdir, do_barcode_demultiplex = FALSE)
 config_file <- "/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/bulk.json"
 config <- jsonlite::fromJSON("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/bulk.json")
--- code/02_quantification/07_flames/02_flames_multisample.sh
+++ code/02_quantification/07_flames/02_flames_multisample.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH -p shared
-#SBATCH --mem=100G
+#SBATCH --mem=150G
 #SBATCH --job-name=flames
 #SBATCH -c 20
 #SBATCH --mail-user=sparthi1@jhu.edu
 #SBATCH --mail-type=ALL
 #SBATCH -o logs/flames_min_count2.%a.txt
 #SBATCH -e logs/flames_min_count2.%a.txt
-#SBATCH --array=1-15
 #SBATCH --time=7-00:00:00
 
 echo "**** Job starts ****"
@@ -17,7 +16,6 @@ echo "User: ${USER}"
 echo "Job id: ${SLURM_JOB_ID}"
 echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${SLURMD_NODENAME}"
-echo "Array task ID: ${SLURM_ARRAY_TASK_ID}"
 
 CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
 sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
@@ -28,7 +26,9 @@ mkdir /dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/$sample/
 # ln -s /dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_assembly/high_quality/${sample}_primary_over_30_sorted.bam /dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/$sample/matched_reads_align2genome.bam
 # ln -s /dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_assembly/high_quality/${sample}_primary_over_30_sorted.bam.bai /dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/$sample/matched_reads_align2genome.bam.bai
 
-
 echo "**** Processing sample $sample ****"
 module load conda_R/4.4.x
-Rscript 01_flames.R $sample
+Rscript 02_flames.R
+
+echo "**** Job ends ****"
+date +"%Y-%m-%d %T"
