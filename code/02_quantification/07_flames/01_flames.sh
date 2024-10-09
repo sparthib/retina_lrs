d968e9c423f5736ebe38dd1af52b55e1e694b5fd
Sowmya Parthiban <sparthi1@jh.edu>
2024-10-09 19:14:28 -0400

    flames bulk multi-sample

--- code/02_quantification/07_flames/01_flames.sh
+++ code/02_quantification/07_flames/01_flames.sh
@@ -24,11 +24,12 @@ sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
 mkdir /dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/$sample/
 
 #create symlink for FASTQ and bam file 
-ln -s /dcs04/hicks/data/sparthib/retina_lrs/03a_nanofilt_fastqs/${sample}.fastq /dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/$sample/matched_reads.fastq
-ln -s /dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_assembly/high_quality/${sample}_primary_over_30_sorted.bam /dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/$sample/matched_reads_align2genome.bam
-ln -s /dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_assembly/high_quality/${sample}_primary_over_30_sorted.bam.bai /dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/$sample/matched_reads_align2genome.bam.bai
+# ln -s /dcs04/hicks/data/sparthib/retina_lrs/03a_nanofilt_fastqs/${sample}.fastq /dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/$sample/matched_reads.fastq
+# ln -s /dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_assembly/high_quality/${sample}_primary_over_30_sorted.bam /dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/$sample/matched_reads_align2genome.bam
+# ln -s /dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_assembly/high_quality/${sample}_primary_over_30_sorted.bam.bai /dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/$sample/matched_reads_align2genome.bam.bai
 
 
 echo "**** Processing sample $sample ****"
 module load conda_R/4.4.x
-Rscript 01_flames.R $sample
+# Rscript 01_flames.R $sample
+Rscript 02_flames.R
--- code/02_quantification/07_flames/02_multisample.R
+++ code/02_quantification/07_flames/02_multisample.R
@@ -1,10 +1,9 @@
 library("FLAMES")
 library("sessioninfo")
 
-sample <- commandArgs(trailingOnly = TRUE)
 gtf <- "/dcs04/hicks/data/sparthib/references/genome/GENCODE/primary_assembly/release_46_primary_assembly.gtf"
 genome_fa  <- "/dcs04/hicks/data/sparthib/references/genome/GENCODE/primary_assembly/release_46_primary_genome.fa"
-outdir <- paste0("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/", sample)
+outdir <- "/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/ROs"
 minimap_path <- file.path("/users/sparthib/minimap2")
 
 #config_file <- FLAMES::create_config(outdir, do_barcode_demultiplex = FALSE)
@@ -12,10 +11,17 @@ config_file <- "/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/b
 config <- jsonlite::fromJSON("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/bulk.json")
 
 se <- bulk_long_pipeline(
-  annotation = gtf, fastq = fastq, outdir = outdir,
+  annotation = gtf, outdir = outdir,
   genome_fa = genome_fa, config_file = config_file
 )
 saveRDS(se, file = paste0(outdir, "/se.rds"))
 
+
+outdir <- "/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/FT_vs_RGC"
+se <- bulk_long_pipeline(
+  annotation = gtf, outdir = outdir,
+  genome_fa = genome_fa, config_file = config_file
+)
+saveRDS(se, file = paste0(outdir, "/se.rds"))
 
 sessioninfo::session_info()
