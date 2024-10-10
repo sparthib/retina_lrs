4f6b85ad0517d3fc227bd78926199b7994fa82da
Sowmya Parthiban <sparthi1@jh.edu>
2024-10-09 20:23:56 -0400

    name fastq file vector

--- code/02_quantification/07_flames/02_flames_multisample.sh
+++ code/02_quantification/07_flames/02_flames_multisample.sh
@@ -26,6 +26,60 @@ mkdir /dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/$sample/
 # ln -s /dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_assembly/high_quality/${sample}_primary_over_30_sorted.bam /dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/$sample/matched_reads_align2genome.bam
 # ln -s /dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_assembly/high_quality/${sample}_primary_over_30_sorted.bam.bai /dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/$sample/matched_reads_align2genome.bam.bai
 
+# RO_samples=("EP1-BRN3B-RO" "EP1-WT_hRO_2" "EP1-WT_ROs_D45" "H9-BRN3B_hRO_2" "H9-BRN3B-RO" "H9-CRX_hRO_2" "H9-CRX_ROs_D45")
+# 
+#   
+# for sample in "${RO_samples[@]}"; do
+#   # Construct source file path
+#   source_file="/dcs04/hicks/data/sparthib/retina_lrs/03a_nanofilt_fastqs/${sample}.fastq"
+#   # Construct target symlink path
+#   target_link="/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/ROs/fastqs/$sample.fastq"
+#   # Create the symlink
+#   ln -s "$source_file" "$target_link"
+#   
+#   # Optional: echo to confirm what's happening
+#   echo "Created symlink for $sample"
+# done
+
+
+# 
+# FT_RGC_samples=("H9-FT_1" "H9-hRGC_1" "H9-FT_2" "H9-hRGC_2" )
+# 
+# for sample in "${FT_RGC_samples[@]}"; do
+#   # Construct source file path
+#   source_file="/dcs04/hicks/data/sparthib/retina_lrs/03a_nanofilt_fastqs/${sample}.fastq"
+#   # Construct target symlink path
+#   target_link="/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/FT_vs_RGC/fastqs/$sample.fastq"
+#   # Create the symlink
+#   ln -s "$source_file" "$target_link"
+#   
+#   # Optional: echo to confirm what's happening
+#   echo "Created symlink for $sample"
+# done
+
+# 
+# FT_RGC_samples=("H9-FT_1" "H9-hRGC_1" "H9-FT_2" "H9-hRGC_2" )
+# 
+# for sample in "${FT_RGC_samples[@]}"; do
+#   # Construct source file path
+#   source_file="/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_assembly/high_quality/${sample}_primary_over_30_sorted.bam"
+#   # Construct target symlink path
+#   target_link="/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/FT_vs_RGC/${sample}_align2genome.bam"
+#   # Create the symlink
+#   ln -s "$source_file" "$target_link"
+#   
+#   # Optional: echo to confirm what's hppening
+#   echo "Created symlink for $sample"
+#   
+#   source_file="/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_assembly/high_quality/${sample}_primary_over_30_sorted.bam.bai"
+#   # Construct target symlink path
+#   target_link="/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/FT_vs_RGC/${sample}_align2genome.bam.bai"
+#   # Create the symlink
+#   ln -s "$source_file" "$target_link"
+#   
+# done
+
+
 echo "**** Processing sample $sample ****"
 module load conda_R/4.4.x
 Rscript 02_multisample.R
--- code/02_quantification/07_flames/02_multisample.R
+++ code/02_quantification/07_flames/02_multisample.R
@@ -10,16 +10,32 @@ minimap_path <- file.path("/users/sparthib/minimap2")
 config_file <- "/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/bulk.json"
 config <- jsonlite::fromJSON("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/bulk.json")
 
+RO_fastq_path = "/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/ROs/fastqs"
+RO_fastqs <- list.files(RO_fastq_path)
+
+#name filepaths in list 
+names(RO_fastqs) <- c("EP1-BRN3B-RO", "EP1-WT_hRO_2", "EP1-WT_ROs_D45",
+                      "H9-BRN3B_hRO_2", "H9-BRN3B-RO", "H9-CRX_hRO_2", "H9-CRX_ROs_D45")
+
+
+
 se <- bulk_long_pipeline(
-  annotation = gtf, outdir = outdir,
+  annotation = gtf, 
+  fastq= RO_fastqs,
+  outdir = outdir,
   genome_fa = genome_fa, config_file = config_file
 )
 saveRDS(se, file = paste0(outdir, "/se.rds"))
 
-
+FT_vs_RGC_fastq_path = "/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/FT_vs_RGC/fastqs/"
+FT_vs_RGC_fastqs <- list.files(FT_vs_RGC_fastq_path)
+names(FT_vs_RGC_fastqs) <- c("H9-FT_1","H9-FT_2" , "H9-hRGC_1" ,"H9-hRGC_2")
+ 
 outdir <- "/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/flames/FT_vs_RGC"
 se <- bulk_long_pipeline(
-  annotation = gtf, outdir = outdir,
+  annotation = gtf,
+  fastq = FT_vs_RGC_fastqs,
+  outdir = outdir,
   genome_fa = genome_fa, config_file = config_file
 )
 saveRDS(se, file = paste0(outdir, "/se.rds"))
