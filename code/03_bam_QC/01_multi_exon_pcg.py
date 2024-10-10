d5101fa5133702b039dbd1b64dd85823bd0f411f
Sowmya Parthiban <sparthi1@jh.edu>
2024-10-10 17:59:14 -0400

    sample spec

--- code/03_bam_QC/01_multi_exon_pcg.py
+++ code/03_bam_QC/01_multi_exon_pcg.py
@@ -8,37 +8,39 @@ import sys
 
 #from https://github.molgen.mpg.de/MayerGroup/NGN3_paper_code/blob/main/ONT_seq_analysis/long_short_comparison/junction_per_read.py
 # Load the GTF file
+# Load the GTF file
 gtf_fn = "/dcs04/hicks/data/sparthib/references/genome/GENCODE/primary_assembly/release_46_primary_assembly.gtf"
-gtf = pr.read_gtf(gtf_fn, as_df = True)
+gtf = pd.read_csv(gtf_fn, sep='\t', header=None, comment='#', 
+                  names=["seqname", "source", "feature", "start", "end", "score", "strand", "frame", "attribute"])
+
 
 curr_gene = ''
 goi = list()
 curr_num_exon = 0
 for i in range(gtf.shape[0]):
-    if gtf.iloc[i,2] == 'gene' and gtf.iloc[i,9] == 'protein_coding':
+    if gtf.iloc[i, 2] == 'gene' and 'protein_coding' in gtf.iloc[i, 8]:  # Adjust to check for 'protein_coding'
         if curr_num_exon > 1:
             goi.append(curr_gene)
-        curr_gene = gtf.iloc[i,8]
+        curr_gene = gtf.iloc[i, 8].split(';')[0]  # Get the gene ID
         curr_num_exon = 0
-    if gtf.iloc[i,2] == 'exon' and gtf.iloc[i,8] == curr_gene:
+    if gtf.iloc[i, 2] == 'exon' and curr_gene in gtf.iloc[i, 8]:
         curr_num_exon += 1
 
-
-genic_gtf = gtf[gtf['Feature'] == 'gene']
-genic_gtf = genic_gtf[genic_gtf['gene_id'].isin(goi)]
+genic_gtf = gtf[gtf['feature'] == 'gene']
+genic_gtf = genic_gtf[genic_gtf['attribute'].str.contains('|'.join(goi))]
 genic_gtf = genic_gtf.iloc[:, 0:14]
 
 ###Get bam file from command line arg
 if len(sys.argv) > 1:
     sample = sys.argv[1]
     print(f"sample: {sample}")
+
     alignment_file = f'/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_assembly/high_quality/{sample}_primary_over_30_chr_only_sorted.bam'
-    alignment = list()
-    alignment.append(alignment_file)
+    alignment = [alignment_file]
     alignment.sort()
 else:
     alignment_dir = '/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_assembly/high_quality/'
-    alignment = list()
+    alignment = []
     for file in os.listdir(alignment_dir):
         if file.endswith("primary_over_30_chr_only_sorted.bam"):
             alignment.append(os.path.join(alignment_dir, file))
@@ -48,40 +50,26 @@ else:
 
 ###Define the function to compute the numebr of exon-exon junctions covered by one read based on CIGAR string
 def compute_num_junction_per_read(cigar_string):
-    #m: number of exon-exon splice junctions
-    m = cigar_string.count('N')
-    return m
-  
-###Calculate for each bam file
-max_junction = 11 #can be changed
-nums = list()
+    return cigar_string.count('N')
+
+# Calculate for each bam file
+max_junction = 11  # Can be changed
+nums = []
 df_dic = {}
-# whole_cigar = ''
+
 for fn in alignment:
-    bamfile = pysam.AlignmentFile(fn, "rb") 
+    bamfile = pysam.AlignmentFile(fn, "rb")
     for i in range(genic_gtf.shape[0]):
-    # for read in bamfile.fetch('chr3', 149964904, 149970895): #PFN2, at most 2 exon-exon junctions
-    # for read in bamfile.fetch('chr14', 69767112, 69772005): #SRSF5, at most 8 exon-exon junctions
-        for read in bamfile.fetch(genic_gtf.iloc[i,0], genic_gtf.iloc[i,3], genic_gtf.iloc[i,4]):  #whole genome
-        #     print(read.mapq)
-        #     print(read)
-        #     break
-    #         name = read.query_name
-    #         names.append(name)
-    #         mq = read.mapq
-    #         mqs.append(mq)
-            num = compute_num_junction_per_read(read.cigarstring)
-            nums.append(num)
-    #         cigar = read.cigarstring
-    #         whole_cigar = whole_cigar + cigar
-    #     print(Counter(nums))
+        contig = genic_gtf.iloc[i, 0]
+        if contig in bamfile.references:  # Check if the contig exists in the BAM file
+            for read in bamfile.fetch(contig, genic_gtf.iloc[i, 3], genic_gtf.iloc[i, 4]):
+                num = compute_num_junction_per_read(read.cigarstring)
+                nums.append(num)
+        
     tmp_counter = Counter(nums)
-    # print(tmp_counter)
-    tmp_num = list()
-    for i in range(max_junction):   
-        tmp_num.append(tmp_counter[i])
-    df_dic[fn] = tmp_num    
-    nums = list()
+    tmp_num = [tmp_counter.get(i, 0) for i in range(max_junction)]  # Handle missing keys
+    df_dic[fn] = tmp_num
+    nums = []
 
 df = pd.DataFrame(df_dic).transpose()
 per_df = df.div(df.sum(axis=1), axis=0)
