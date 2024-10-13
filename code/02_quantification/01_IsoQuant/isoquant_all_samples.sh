bcaf7ed315428380935f4c8d7acd5e19fa1c6183
Sowmya Parthiban <sparthi1@jh.edu>
2024-10-13 10:49:35 -0400

    run Isoquant for all samples

---
+++ code/02_quantification/01_IsoQuant/isoquant_all_samples.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH -p shared
+#SBATCH --mem=350G
+#SBATCH --cpus-per-task=20
+#SBATCH --job-name=isoquant_RO
+#SBATCH --mail-user=sparthi1@jhu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o logs/isoquant_all_samples.txt
+#SBATCH -e logs/isoquant_all_samples.txt
+#SBATCH --time=7-00:00:00
+
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+
+source activate isoquant 
+
+CONFIG=/users/sparthib/retina_lrs/raw_data/data_paths.config
+sample=$(awk -v Index=$SLURM_ARRAY_TASK_ID '$1==Index {print $2}' $CONFIG)
+echo "${sample}"
+BAM_FOLDER=/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_assembly/high_quality
+REFERENCE_GTF=/dcs04/hicks/data/sparthib/references/genome/GENCODE/primary_assembly/release_46_primary_assembly.gtf.gz
+REFERENCE_FASTA=/dcs04/hicks/data/sparthib/references/genome/GENCODE/primary_assembly/release_46_primary_genome.fa.gz
+
+EP1_BRN3B_RO=$BAM_FOLDER/EP1-BRN3B-RO_primary_over_30_sorted.bam
+EP1_WT_hRO_2=$BAM_FOLDER/EP1-WT_hRO_2_primary_over_30_sorted.bam
+EP1_WT_ROs_D45=$BAM_FOLDER/EP1-WT_ROs_D45_primary_over_30_sorted.bam
+H9_BRN3B_hRO_2=$BAM_FOLDER/H9-BRN3B_hRO_2_primary_over_30_sorted.bam
+H9_BRN3B_RO=$BAM_FOLDER/H9-BRN3B-RO_primary_over_30_sorted.bam
+H9_CRX_hRO_2=$BAM_FOLDER/H9-CRX_hRO_2_primary_over_30_sorted.bam
+H9_CRX_ROs_D45=$BAM_FOLDER/H9-CRX_ROs_D45_primary_over_30_sorted.bam
+H9_FT_1=$BAM_FOLDER/H9-FT_1_primary_over_30_sorted.bam
+H9_FT_2=$BAM_FOLDER/H9-FT_2_primary_over_30_sorted.bam
+H9_hRGC_1=$BAM_FOLDER/H9-hRGC_1_primary_over_30_sorted.bam
+H9_hRGC_2=$BAM_FOLDER/H9-hRGC_2_primary_over_30_sorted.bam
+
+OUTPUT_FOLDER=/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/isoquant/high_quality/all_samples
+mkdir -p $OUTPUT_FOLDER
+
+isoquant.py --reference $REFERENCE_FASTA --data_type ont --genedb $REFERENCE_GTF \
+--bam $EP1_BRN3B_RO $EP1_WT_hRO_2 $EP1_WT_ROs_D45 $H9_BRN3B_hRO_2 $H9_BRN3B_RO $H9_CRX_hRO_2 $H9_CRX_ROs_D45 $H9_FT_1 $H9_FT_2 $H9_hRGC_1 $H9_hRGC_2  \
+  --output $OUTPUT_FOLDER --clean_start --count_exons -t ${SLURM_CPUS_PER_TASK} \
+  --complete_genedb --sqanti_output --check_canonical
+  
+  
+conda deactivate
+
+echo "**** Job ends ****"
+date +"%Y-%m-%d %T"
