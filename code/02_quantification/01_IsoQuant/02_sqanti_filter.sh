9b235306d6d11ae324de19a7669c1baeadbfb568
Sowmya Parthiban <sparthi1@jh.edu>
2024-10-15 13:43:28 -0400

    run SQANTI filter

---
+++ code/02_quantification/01_IsoQuant/02_sqanti_filter.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH --mem=200G
+#SBATCH -c 15
+#SBATCH --job-name=sq_filter
+#SBATCH --mail-user=sparthi1@jhu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o logs/sq_filter.txt
+#SBATCH -e logs/sq_filter.txt
+#SBATCH --time=7-00:00:00
+
+
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "****"
+
+source activate /users/sparthib/.conda/envs/SQANTI3
+SQANTI_DIR=~/SQANTI3-5.2.1
+
+INPUT_DIR=/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/isoquant/high_quality/all_samples/sqanti3_qc
+
+python $SQANTI_DIR/sqanti3_filter.py ML --gtf $INPUT_DIR/all_samples_corrected.gtf \
+ -d $INPUT_DIR/SQANTI3_ML_out --filter_mono_exonic \
+$INPUT_DIR/all_samples_classification.txt
+
+
+echo "**** Job ends ****"
+    
