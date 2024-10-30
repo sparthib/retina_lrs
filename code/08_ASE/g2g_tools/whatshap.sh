92887fadbbdb66bcd069546e65879825ae975745
Sowmya Parthiban <sparthi1@jh.edu>
2024-10-30 14:04:28 -0400

    try hp bam with whatshap

--- code/08_ASE/g2g_tools/whatshap.sh
+++ code/08_ASE/g2g_tools/whatshap.sh
@@ -26,10 +26,10 @@ genome_bam_dir=/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_asse
 transcriptome_bam_dir=/dcs04/hicks/data/sparthib/retina_lrs/05_bams/transcriptome/ver_46
 
 ml load bcftools 
-bcftools view -g ^miss -i 'GT[*]~"|"' $vcf_dir/SRR1091088.genotyped.vcf -o $vcf_dir/SRR1091088.phased.vcf
+# bcftools view -g ^miss -i 'GT[*]~"|"' $vcf_dir/SRR1091088.genotyped.vcf -o $vcf_dir/SRR1091088.phased.vcf
 
-# whatshap haplotag -o haplotagged.bam --reference $ref_fa \
-# $vcf_dir/SRR1091088.phased.vcf $genome_bam_dir/H9-hRGC_1_sorted.bam
+whatshap haplotag -o $vcf_dir/H9-hRGC_1_sorted_haplotagged.bam --reference $ref_fa \
+$vcf_dir/SRR1091088.phased.vcf $genome_bam_dir/H9-hRGC_1_sorted.bam
 
 
 
