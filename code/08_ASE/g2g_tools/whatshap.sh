f66688a1dac78c4e0e5136d8dbcf02c8f728820e
Sowmya Parthiban <sparthi1@jh.edu>
2024-10-30 14:11:40 -0400

    zip vcf

--- code/08_ASE/g2g_tools/whatshap.sh
+++ code/08_ASE/g2g_tools/whatshap.sh
@@ -28,7 +28,9 @@ transcriptome_bam_dir=/dcs04/hicks/data/sparthib/retina_lrs/05_bams/transcriptom
 ml load bcftools 
 # bcftools view -g ^miss -i 'GT[*]~"|"' $vcf_dir/SRR1091088.genotyped.vcf -o $vcf_dir/SRR1091088.phased.vcf
 ml load  htslib
-tabix -p vcf $vcf_dir/SRR1091088.phased.vcf
+
+bgzip -c $vcf_dir/SRR1091088.phased.vcf > $vcf_dir/SRR1091088.phased.vcf.gz
+tabix -p vcf $vcf_dir/SRR1091088.phased.vcf.gz
 
 whatshap haplotag -o $vcf_dir/H9-hRGC_1_sorted_haplotagged.bam --reference $ref_fa \
 $vcf_dir/SRR1091088.phased.vcf $genome_bam_dir/H9-hRGC_1_sorted.bam \
