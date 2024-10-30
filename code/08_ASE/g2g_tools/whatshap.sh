be28c5b742a5f9468d2a0c7582d56fe9b09e543a
Sowmya Parthiban <sparthi1@jh.edu>
2024-10-30 14:06:58 -0400

    ignore read groups

--- code/08_ASE/g2g_tools/whatshap.sh
+++ code/08_ASE/g2g_tools/whatshap.sh
@@ -29,7 +29,8 @@ ml load bcftools
 # bcftools view -g ^miss -i 'GT[*]~"|"' $vcf_dir/SRR1091088.genotyped.vcf -o $vcf_dir/SRR1091088.phased.vcf
 
 whatshap haplotag -o $vcf_dir/H9-hRGC_1_sorted_haplotagged.bam --reference $ref_fa \
-$vcf_dir/SRR1091088.phased.vcf $genome_bam_dir/H9-hRGC_1_sorted.bam
+$vcf_dir/SRR1091088.phased.vcf $genome_bam_dir/H9-hRGC_1_sorted.bam \
+--ignore-read-groups
 
 
 
