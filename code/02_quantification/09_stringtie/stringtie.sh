bbb77fb971b315d6a145ac8054df38de25f3235e
Sowmya Parthiban <sparthi1@jh.edu>
2024-11-23 20:48:59 -0500

    remove merge

--- code/02_quantification/09_stringtie/stringtie.sh
+++ code/02_quantification/09_stringtie/stringtie.sh
@@ -37,4 +37,4 @@ OUTPUT_FILE="${OUTPUT_FOLDER}/${SAMPLE}.gtf"
 
 # Run stringtie
 echo "Running stringtie for sample: $SAMPLE"
-~/stringtie/stringtie -o $OUTPUT_FILE --merge -L -G $REFERENCE_GTF $BAM_FILE
+~/stringtie/stringtie -L -G $REFERENCE_GTF -o $OUTPUT_FILE  $BAM_FILE
