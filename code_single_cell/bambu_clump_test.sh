a78fe6d1e7647ee5a9bc9a11f93e073f69bcb4eb
Sowmya Parthiban <sparthi1@jh.edu>
2025-02-19 15:37:27 -0500

    update nextflow dir

--- code_single_cell/bambu_clump_test.sh
+++ code_single_cell/bambu_clump_test.sh
@@ -33,7 +33,8 @@ nextflow run GoekeLab/bambu-singlecell-spatial \
   --annotation $reference_gtf \
   --chemistry 10x3v2 \
   --ncore 19 --outdir $output_dir \
-  -with-singularity lingminhao/bambusc:beta1.2
+  -with-singularity lingminhao/bambusc:beta1.2 \
+  -w $output_dir
   
 echo "****Job Ends****"
 date +"%Y-%m-%d %T"
