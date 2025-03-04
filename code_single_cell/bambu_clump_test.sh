4d54453984d4d3e2aba655957c3ab124c689a549
Sowmya Parthiban <sparthi1@jh.edu>
2025-03-03 21:44:31 -0500

    run nextflow

--- code_single_cell/bambu_clump_test.sh
+++ code_single_cell/bambu_clump_test.sh
@@ -26,8 +26,9 @@ echo "Job id: ${SLURM_JOB_ID}"
 echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${SLURMD_NODENAME}"
 
-singularity pull docker://lingminhao/bambusc:beta1.2
-nextflow run /dcs04/hicks/data/sparthib/retina_single_cell_lrs/05c_bambu_clump/bambu-singlecell-spatial \
+# singularity pull docker://lingminhao/bambusc:beta1.2
+# nextflow run /dcs04/hicks/data/sparthib/retina_single_cell_lrs/05c_bambu_clump/bambu-singlecell-spatial \
+nextflow run GoekeLab/bambu-singlecell-spatial \
   --bams /users/sparthib/retina_lrs/raw_data/single_cell_samples.csv \
   --genome $reference_dir \
   --annotation $reference_gtf \
