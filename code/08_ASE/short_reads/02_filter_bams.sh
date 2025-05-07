53ba8e8a28ed4271a7e026fefe5f350269230078
Sowmya Parthiban <sparthi1@jh.edu>
2025-05-07 13:59:18 -0400

    filter bams

--- code/08_ASE/short_reads/02_filter_bams.sh
+++ code/08_ASE/short_reads/02_filter_bams.sh
@@ -50,7 +50,7 @@ ml load samtools
 ## first mark duplicates, then filter by MAPQ and uniquely mapped reads 
 
 # samples=(SRR1091088 SRR1091091 SRR1091092)
-samples=(SRR1091092)
+samples=(SRR1091091)
 mkdir -p "$OUTPUT_DIR"
 
 for sample in "${samples[@]}"
