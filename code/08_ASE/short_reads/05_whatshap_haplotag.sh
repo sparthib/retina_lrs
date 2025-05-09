3fb47fd8af080ea70bcb73cd91d0010ae492aecb
Sowmya Parthiban <sparthi1@jh.edu>
2025-05-09 10:49:47 -0400

    update haplotag and split code

--- code/08_ASE/short_reads/05_whatshap_haplotag.sh
+++ code/08_ASE/short_reads/05_whatshap_haplotag.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+#!/bin/bash
+
 #SBATCH -p shared
-#SBATCH --nodes=1
-#SBATCH --mem=50G
-#SBATCH -c 20
-#SBATCH --job-name=whatshap_haplotag
+#SBATCH --mem=40G
+#SBATCH -c 10
+#SBATCH --job-name=haplotag
 #SBATCH --mail-user=sparthi1@jhu.edu
 #SBATCH --mail-type=ALL
-#SBATCH -o logs/whatshap_haplotag.txt
-#SBATCH -e logs/whatshap_haplotag.txt
+#SBATCH -o logs/haplotag.%a.txt
+#SBATCH -e logs/haplotag.%a.txt
+#SBATCH --array=1-8
 #SBATCH --time=7-00:00:00
-
 
 ### whatshap phases the variants we found using GATK with the help 
 ### of our long-read BAMs
@@ -29,7 +30,6 @@ ref_fa=/dcs04/hicks/data/sparthib/references/genome/GENCODE/primary_assembly/rel
 vcf_dir=/dcs04/hicks/data/sparthib/retina_lrs/09_ASE/H9_DNA_Seq_data/gvcf_ref_46
 genome_bam_dir=/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_assembly/high_quality
 whatshap_output_dir=/dcs04/hicks/data/sparthib/retina_lrs/09_ASE/H9_DNA_Seq_data/whatshap_output
-mkdir -p $whatshap_output_dir
 
 source activate whatshap-env
 
@@ -37,42 +37,23 @@ source activate whatshap-env
 
 ## input vcf file needs to be indexed prior to running whatshap
 # used bgzip from htslib module for zipping vcf and then tabix to index
-ml load htslib
-bgzip -c $whatshap_output_dir/phased.vcf > $whatshap_output_dir/phased.vcf.gz
-tabix -p vcf $whatshap_output_dir/phased.vcf.gz
+# ml load htslib
+# bgzip -c $whatshap_output_dir/phased.vcf > $whatshap_output_dir/phased.vcf.gz
+# tabix -p vcf $whatshap_output_dir/phased.vcf.gz
 
+samples=(H9-BRN3B_hRO_2 H9-BRN3B-RO H9-CRX_hRO_2 H9-CRX_ROs_D45 H9-FT_1 H9-FT_2 H9-hRGC_1 H9-hRGC_2) 
 
-whatshap haplotag -o $whatshap_output_dir/H9-BRN3B_hRO_2_primary_over_30_chr_only_sorted.bam \
---reference $ref_fa $whatshap_output_dir/phased.vcf.gz $genome_bam_dir/H9-BRN3B_hRO_2_primary_over_30_chr_only_sorted.bam \
---output-threads=19 --ignore-read-groups --sample=SRR1091091
+lr_sample=${samples[$SLURM_ARRAY_TASK_ID]}
 
-whatshap haplotag -o $whatshap_output_dir/H9-BRN3B-RO_primary_over_30_chr_only_sorted.bam \
---reference $ref_fa $whatshap_output_dir/phased.vcf.gz $genome_bam_dir/H9-BRN3B-RO_primary_over_30_chr_only_sorted.bam \
---output-threads=19 --ignore-read-groups --sample=SRR1091091
+echo "Processing sample: $lr_sample"
 
-whatshap haplotag -o $whatshap_output_dir/H9-CRX_hRO_2_primary_over_30_chr_only_sorted.bam \
---reference $ref_fa $whatshap_output_dir/phased.vcf.gz $genome_bam_dir/H9-CRX_hRO_2_primary_over_30_chr_only_sorted.bam \
---output-threads=19 --ignore-read-groups --sample=SRR1091091
+whatshap haplotag -o $whatshap_output_dir/${lr_sample}.bam \
+--reference $ref_fa $whatshap_output_dir/phased.vcf.gz $genome_bam_dir/${lr_sample}_primary_over_30_chr_only_sorted.bam \
+--output-threads=19 --ignore-read-groups --sample=SRR1091091 --output-haplotag-list $whatshap_output_dir/${lr_sample}_haplotypes.tsv
 
-whatshap haplotag -o $whatshap_output_dir/H9-CRX_ROs_D45_primary_over_30_chr_only_sorted.bam \
---reference $ref_fa $whatshap_output_dir/phased.vcf.gz $genome_bam_dir/H9-CRX_ROs_D45_primary_over_30_chr_only_sorted.bam \
---output-threads=19 --ignore-read-groups --sample=SRR1091091
+whatshap split --output-h1 $whatshap_output_dir/${lr_sample}_h1.bam \
+--output-h2 $whatshap_output_dir/${lr_sample}_h2.bam $whatshap_output_dir/${lr_sample}.bam $whatshap_output_dir/${lr_sample}_haplotypes.tsv
 
-whatshap haplotag -o $whatshap_output_dir/H9-FT_1_primary_over_30_chr_only_sorted.bam \
---reference $ref_fa $whatshap_output_dir/phased.vcf.gz $genome_bam_dir/H9-FT_1_primary_over_30_chr_only_sorted.bam \
---output-threads=19 --ignore-read-groups --sample=SRR1091091
-
-whatshap haplotag -o $whatshap_output_dir/H9-FT_2_primary_over_30_chr_only_sorted.bam \
---reference $ref_fa $whatshap_output_dir/phased.vcf.gz $genome_bam_dir/H9-FT_2_primary_over_30_chr_only_sorted.bam \
---output-threads=19 --ignore-read-groups --sample=SRR1091091
-
-whatshap haplotag -o $whatshap_output_dir/H9-hRGC_1_primary_over_30_chr_only_sorted.bam \
---reference $ref_fa $whatshap_output_dir/phased.vcf.gz $genome_bam_dir/H9-hRGC_1_primary_over_30_chr_only_sorted.bam \
---output-threads=19 --ignore-read-groups --sample=SRR1091091
-
-whatshap haplotag -o $whatshap_output_dir/H9-hRGC_2_primary_over_30_chr_only_sorted.bam \
---reference $ref_fa $whatshap_output_dir/phased.vcf.gz $genome_bam_dir/H9-hRGC_2_primary_over_30_chr_only_sorted.bam \
---output-threads=19 --ignore-read-groups --sample=SRR1091091
 
 
 echo "**** Job ends ****"
