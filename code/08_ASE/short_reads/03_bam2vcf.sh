8ff93fc6d484cb9616a892dd21dc778887c969db
Sowmya Parthiban <sparthi1@jh.edu>
2025-06-13 17:08:05 -0400

    update bam to VCF code

--- code/08_ASE/short_reads/03_bam2vcf.sh
+++ code/08_ASE/short_reads/03_bam2vcf.sh
@@ -32,47 +32,46 @@ mkdir -p $OUTPUT_DIR
 ml load gatk
 samples=(SRR1091088 SRR1091091)
 
-# echo "🔹 Step 1: Haplotype calling to produce gVCF"
-# for sample in "${samples[@]}"
-# do 
-# gatk HaplotypeCaller \
-#     -R "$ref_fa" \
-#     -I "${INPUT_DIR}/${sample}_filtered.bam" \
-#     -O "${OUTPUT_DIR}/${sample}.g.vcf.gz" \
-#     -ERC GVCF
-# done
-  
-echo "🔹 Step 2: Combine gVCFs from multiple samples"
-gatk CombineGVCFs \
-  -R "$ref_fa" \
-  $(for sample in "${samples[@]}"; do echo -n "-V ${OUTPUT_DIR}/${sample}.g.vcf.gz "; done) \
-  -O "${OUTPUT_DIR}/combined.g.vcf.gz"
+echo "🔹 Step 1: Haplotype calling to produce gVCF"
+gatk HaplotypeCaller \
+    -R "$ref_fa" \
+    -I "${INPUT_DIR}/all_samples_filtered.bam" \
+    -O "${OUTPUT_DIR}/all_samples.g.vcf.gz" \
+    -ERC GVCF
+    
+# Uncomment the following lines if you want to run HaplotypeCaller for each sample individually
+# echo "🔹 Step 2: Combine gVCFs from multiple samples"
+# gatk CombineGVCFs \
+#   -R "$ref_fa" \
+#   $(for sample in "${samples[@]}"; do echo -n "-V ${OUTPUT_DIR}/${sample}.g.vcf.gz "; done) \
+#   -O "${OUTPUT_DIR}/combined.g.vcf.gz"
 
 echo "🔹 Step 3: Genotype the combined gVCF"
 gatk GenotypeGVCFs \
   -R "$ref_fa" \
-  -V "${OUTPUT_DIR}/combined.g.vcf.gz" \
-  -O "${OUTPUT_DIR}/combined.vcf.gz"
+  -V "${OUTPUT_DIR}/all_samples.g.vcf.gz" \
+  -O "${OUTPUT_DIR}/all_samples.vcf.gz"
+
 
 echo "🔹 Step 4a: Split VCF into SNPs and INDELs"
 
 gatk SelectVariants \
   -R "$ref_fa" \
-  -V "${OUTPUT_DIR}/combined.vcf.gz" \
+  -V "${OUTPUT_DIR}/all_samples.vcf.gz" \
   --select-type-to-include SNP \
-  -O "${OUTPUT_DIR}/combined_SNPs.vcf.gz"
+  -O "${OUTPUT_DIR}/all_samples_SNPs.vcf.gz"
 
 gatk SelectVariants \
   -R "$ref_fa" \
-  -V "${OUTPUT_DIR}/combined.vcf.gz" \
+  -V "${OUTPUT_DIR}/all_samples.vcf.gz" \
   --select-type-to-include INDEL \
-  -O "${OUTPUT_DIR}/combined_INDELs.vcf.gz"
+  -O "${OUTPUT_DIR}/all_samples_INDELs.vcf.gz"
  
  echo "🔹 Step 4b: Filter SNPs and INDELs" 
  
   gatk VariantFiltration \
   -R "$ref_fa" \
-  -V "${OUTPUT_DIR}/combined_SNPs.vcf.gz" \
+  -V "${OUTPUT_DIR}/all_samples_SNPs.vcf.gz" \
    -filter "QD < 2.0" --filter-name "QD2" \
    -filter "QUAL < 30.0" --filter-name "QUAL30" \
    -filter "SOR > 3.0" --filter-name "SOR3" \
@@ -80,36 +79,36 @@ gatk SelectVariants \
    -filter "MQ < 40.0" --filter-name "MQ40" \
    -filter "MQRankSum < -12.5" --filter-name "MQRankSum-12.5" \
    -filter "ReadPosRankSum < -8.0" --filter-name "ReadPosRankSum-8" \
-  -O "${OUTPUT_DIR}/combined_SNPs_filtered.vcf.gz"
+  -O "${OUTPUT_DIR}/all_samples_SNPs_filtered.vcf.gz"
   
 gatk VariantFiltration \
   -R "$ref_fa" \
-  -V "${OUTPUT_DIR}/combined_INDELs.vcf.gz" \
+  -V "${OUTPUT_DIR}/all_samples_INDELs.vcf.gz" \
       -filter "QD < 2.0" --filter-name "QD2" \
     -filter "QUAL < 30.0" --filter-name "QUAL30" \
     -filter "FS > 200.0" --filter-name "FS200" \
     -filter "ReadPosRankSum < -20.0" --filter-name "ReadPosRankSum-20" \
-    -O "${OUTPUT_DIR}/combined_INDELs_filtered.vcf.gz"
+    -O "${OUTPUT_DIR}/all_samples_INDELs_filtered.vcf.gz"
   
   
   echo "🔹 Step 4c: only keep the PASS variants from the filtered files"
 gatk SelectVariants \
   -R "$ref_fa" \
-  -V "${OUTPUT_DIR}/combined_SNPs_filtered.vcf.gz" \
+  -V "${OUTPUT_DIR}/all_samples_SNPs_filtered.vcf.gz" \
   --exclude-filtered \
-  -O "${OUTPUT_DIR}/combined_SNPs_filtered_PASS.vcf.gz"  
+  -O "${OUTPUT_DIR}/all_samples_SNPs_filtered_PASS.vcf.gz"  
   
 gatk SelectVariants \
   -R "$ref_fa" \
-  -V "${OUTPUT_DIR}/combined_INDELs_filtered.vcf.gz" \
+  -V "${OUTPUT_DIR}/all_samples_INDELs_filtered.vcf.gz" \
   --exclude-filtered \
-  -O "${OUTPUT_DIR}/combined_INDELs_filtered_PASS.vcf.gz"
+  -O "${OUTPUT_DIR}/all_samples_INDELs_filtered_PASS.vcf.gz"
   
 echo "🔹 Step 5: Merge SNPs and INDELs"
 gatk MergeVcfs \
-  -I "${OUTPUT_DIR}/combined_SNPs_filtered_PASS.vcf.gz" \
-  -I "${OUTPUT_DIR}/combined_INDELs_filtered_PASS.vcf.gz" \
-  -O "${OUTPUT_DIR}/merged_variants.vcf.gz"
+  -I "${OUTPUT_DIR}/all_samples_SNPs_filtered_PASS.vcf.gz" \
+  -I "${OUTPUT_DIR}/all_samples_INDELs_filtered_PASS.vcf.gz" \
+  -O "${OUTPUT_DIR}/all_samples_variants.vcf.gz"
 
 echo "**** Job ends ****"
 date +"%Y-%m-%d %T"
