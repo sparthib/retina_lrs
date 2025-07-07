cf1321d3b7a611d75ac46426436cd874e9c30986
Sowmya Parthiban <sparthi1@jh.edu>
2025-07-07 14:05:56 -0400

    update haplotag

--- code/08_ASE/short_reads/05_whatshap_haplotag.sh
+++ code/08_ASE/short_reads/05_whatshap_haplotag.sh
@@ -30,7 +30,9 @@ ref_fa=/dcs04/hicks/data/sparthib/references/genome/GENCODE/primary_assembly/rel
 vcf_dir=/dcs04/hicks/data/sparthib/retina_lrs/09_ASE/H9_DNA_Seq_data/gvcf_ref_46
 genome_bam_dir=/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_assembly/high_quality
 phased_vcf=/dcs04/hicks/data/sparthib/retina_lrs/09_ASE/H9_DNA_Seq_data/whatshap_output/all_samples_phased.vcf.gz
+phased_vcf_H9_EP1=/dcs04/hicks/data/sparthib/retina_lrs/09_ASE/H9_DNA_Seq_data/whatshap_output/all_samples_H9_and_EP1_phased.vcf
 whatshap_output_dir=/dcs04/hicks/data/sparthib/retina_lrs/09_ASE/H9_DNA_Seq_data/whatshap_output_single_sample
+whatshap_output_dir_H9_EP1=/dcs04/hicks/data/sparthib/retina_lrs/09_ASE/H9_DNA_Seq_data/whatshap_output_phased_on_H9_and_EP1
 mkdir -p $whatshap_output_dir
 
 source activate whatshap-env
@@ -43,18 +45,18 @@ source activate whatshap-env
 # bgzip -c $phased_vcf > $phased_vcf.gz
 # tabix -p vcf $phased_vcf.gz
 
-samples=(H9-BRN3B_hRO_2 H9-BRN3B-RO H9-CRX_hRO_2 H9-CRX_ROs_D45 H9-FT_1 H9-FT_2 H9-hRGC_1 H9-hRGC_2) 
+samples=(H9-BRN3B_hRO_2 H9-BRN3B-RO H9-CRX_hRO_2 H9-CRX_ROs_D45 H9-FT_1 H9-FT_2 H9-hRGC_1 H9-hRGC_2 EP1-BRN3B-RO EP1-WT_hRO_2 EP1-WT_ROs_D45) 
 
 lr_sample=${samples[$SLURM_ARRAY_TASK_ID - 1]}
 
 echo "**** Haplotagging sample: $lr_sample ****"
-whatshap haplotag -o $whatshap_output_dir/${lr_sample}.bam \
---reference $ref_fa $phased_vcf $genome_bam_dir/${lr_sample}_primary_over_30_chr_only_sorted.bam \
---output-threads=19 --ignore-read-groups --output-haplotag-list $whatshap_output_dir/${lr_sample}_haplotypes.tsv
+whatshap haplotag -o $whatshap_output_dir_H9_EP1/${lr_sample}.bam \
+--reference $ref_fa $phased_vcf_H9_EP1 $genome_bam_dir/${lr_sample}_primary_over_30_chr_only_sorted.bam \
+--output-threads=19 --ignore-read-groups --output-haplotag-list $whatshap_output_dir_H9_EP1/${lr_sample}_haplotypes.tsv
 
 echo "**** Splitting haplotagged BAM into haplotypes ****"
-whatshap split --output-h1 $whatshap_output_dir/${lr_sample}_h1.bam \
---output-h2 $whatshap_output_dir/${lr_sample}_h2.bam $whatshap_output_dir/${lr_sample}.bam $whatshap_output_dir/${lr_sample}_haplotypes.tsv
+whatshap split --output-h1 $whatshap_output_dir_H9_EP1/${lr_sample}_h1.bam \
+--output-h2 $whatshap_output_dir_H9_EP1/${lr_sample}_h2.bam $whatshap_output_dir_H9_EP1/${lr_sample}.bam $whatshap_output_dir_H9_EP1/${lr_sample}_haplotypes.tsv
 
 
 
