0f8c815f867c2c21becedc60520f4db0321b5959
Sowmya Parthiban <sparthi1@jh.edu>
2025-07-22 13:49:24 -0400

    update array ID

--- code/08_ASE/short_reads/08b_num_variants_per_read.R
+++ code/08_ASE/short_reads/08b_num_variants_per_read.R
@@ -10,12 +10,15 @@ vcf <- readVcf("/dcs04/hicks/data/sparthib/retina_lrs/09_ASE/H9_DNA_Seq_data/wha
 variants <- rowRanges(vcf)
 
 # Define samples
+array_id <- as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
+
 samples <- c(
   "H9-BRN3B_hRO_2", "H9-BRN3B-RO", "H9-CRX_hRO_2", "H9-CRX_ROs_D45",
   "H9-FT_1", "H9-FT_2", "H9-hRGC_1", "H9-hRGC_2",
   "EP1-BRN3B-RO", "EP1-WT_hRO_2", "EP1-WT_ROs_D45"
 )
 
+sample <- samples[array_id]
 # Define BAM directory
 genome_bam_dir <- "/dcs04/hicks/data/sparthib/retina_lrs/05_bams/genome/primary_assembly/high_quality"
 
@@ -26,7 +29,7 @@ bam_files <- file.path(genome_bam_dir, paste0(samples, "_primary_over_30_chr_onl
 print(bam_files)
 
 # Extract reads and their positions
-bam <- BamFile(bam_files[7])
+bam <- BamFile(bam_files[array_id])
 reads <- readGAlignments(bam)
 
 # Find overlaps
---
+++ code/08_ASE/short_reads/08b_num_variants_per_read.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH --mem=30G
+#SBATCH -c 20
+#SBATCH --job-name=num_var_per_read
+#SBATCH --mail-user=sparthi1@jhu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o logs/num_var_per_read.%a.txt
+#SBATCH -e logs/num_var_per_read.%a.txt
+#SBATCH --time=7-00:00:00
+#SBATCH --array=1-11
+
+echo "**** Job starts ****"
+date +"%Y-%m-%d %T"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+echo "Array ID: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R/4.3.x
+# Rscript 06_gene_counts.R 
+Rscript 08b_num_variants_per_read.R 
+
+echo "**** Job ends ****"
+date
