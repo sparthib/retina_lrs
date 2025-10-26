85b7b3fb00d0bc8562a8bcb2ba17792e9012c243
Sowmya Parthiban <sparthi1@jh.edu>
2025-10-25 20:09:04 -0400

    save annotations as rds

--- code/08_ASE/short_reads/bambu/01_quantification.R
+++ code/08_ASE/short_reads/bambu/01_quantification.R
@@ -4,9 +4,14 @@ library(readr)
 library(sessioninfo)
 library(dplyr)
 
-annotation <- readRDS("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/annotations.rds")
+# gtf_file <- "/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/bambu/all_samples_extended_annotation_track_reads/extended_annotations.gtf"
+# annotations <- prepareAnnotations(gtf_file)
+# outdir <- "/dcs04/hicks/data/sparthib/retina_lrs/09_ASE/H9_DNA_Seq_data/H9_EP1_bambu"
+# saveRDS(annotations, file.path(outdir, "annotations.rds"))
+
+annotation <- readRDS(file.path(outdir, "annotations.rds"))
+
 fa.file <- "/dcs04/hicks/data/sparthib/references/genome/GENCODE/primary_assembly/release_46_primary_genome.fa"
-outdir <- "/dcs04/hicks/data/sparthib/retina_lrs/09_ASE/H9_DNA_Seq_data/H9_EP1_bambu"
 
 sample <- commandArgs(trailingOnly = TRUE)
 
