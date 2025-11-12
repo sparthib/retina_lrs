a35e9183e558f5cd4d269a730668014adf8c0295
Sowmya Parthiban <sparthi1@jh.edu>
2025-11-12 18:11:20 -0500

    update env variables

--- code/03_quantification/IsoQuant/filter_gtf_based_bambu_comparison.R
+++ code/03_quantification/IsoQuant/filter_gtf_based_bambu_comparison.R
@@ -3,13 +3,17 @@
 library(rtracklayer)
 library(dplyr)
 
+data_dir <- Sys.getenv("retina_lrs_dir")
+code_dir <- Sys.getenv("retina_lrs_code")
+ref_dir <- Sys.getenv("references_dir")
+
 # Define file paths
-isoquant_GTF <- file.path("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/isoquant/high_quality/all_samples/OUT",
-                          "OUT.transcript_models.gtf")
+isoquant_GTF <- file.path(data_dir ,"/06_quantification/isoquant/high_quality/all_samples/OUT/OUT.extended_annotation.gtf")
+
 
 method <- "Isoquant"
 comparison <- "ROs"
-matrix_dir <- file.path("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/counts_matrices/",
+matrix_dir <- file.path(data_dir,"06_quantification/counts_matrices/",
                         method, comparison, "filtered")
 counts <- file.path(matrix_dir, "isoform_counts.RDS") 
 
@@ -18,7 +22,10 @@ counts <- readRDS(counts)
 
 # Read GTF file
 gtf <- import(isoquant_GTF)
-
+gtf <- as.data.frame(gtf)
+gtf |>
+  filter(!str_starts(gene_id, "ENSG")) |>
+  distinct(gene_id) |> nrow()
 # Extract isoform IDs from the counts matrix
 isoform_ids <- rownames(counts)
 
@@ -26,7 +33,7 @@ isoform_ids <- rownames(counts)
 filtered_gtf <- subset(gtf, gtf$transcript_id %in% isoform_ids)
 
 # Save filtered GTF to a new file
-filtered_gtf_file <- file.path("/dcs04/hicks/data/sparthib/retina_lrs/06_quantification/isoquant/high_quality/all_samples/OUT", "ROs_filtered_isoforms.gtf")
+filtered_gtf_file <- file.path(data_dir,"06_quantification/isoquant/high_quality/all_samples/OUT", "ROs_filtered_isoforms.gtf")
 export(filtered_gtf, con = filtered_gtf_file)
 
 # Message to indicate success
